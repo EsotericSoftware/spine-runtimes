3dc750be8d2a7dd5fa7651c26b326c582db22d7b
NathanSweet <nathan.sweet@gmail.com>
2013-03-14 22:59:55 +0100

    We'll stay with LWJGL backend for the examples.

--- spine-libgdx/src/com/esotericsoftware/spine/AttachmentLoader.java
+++ spine-libgdx/src/com/esotericsoftware/spine/AttachmentLoader.java
@@ -2,5 +2,6 @@
 package com.esotericsoftware.spine;
 
 public interface AttachmentLoader {
+	/** @return May be null to not load any attachment. */
 	public Attachment newAttachment (AttachmentType type, String name);
 }
