d6e7ae7a4aa406be22870c92e910ef3fd559c215
NathanSweet <nathan.sweet@gmail.com>
2013-03-22 12:13:34 +0100

    Fixed test files.

--- spine-libgdx/src/com/esotericsoftware/spine/AnimationState.java
+++ spine-libgdx/src/com/esotericsoftware/spine/AnimationState.java
@@ -26,16 +26,18 @@
 package com.esotericsoftware.spine;
 
 import com.badlogic.gdx.math.MathUtils;
-import com.badlogic.gdx.utils.ObjectMap;
 
 /** Stores mixing times between animations and automatically mixes them as animations change. */
 public class AnimationState {
+	private final AnimationStateData data;
 	Animation current, previous;
 	float currentTime, previousTime;
 	boolean currentLoop, previousLoop;
 	float mixTime, mixDuration;
-	final ObjectMap<Key, Float> animationToMixTime = new ObjectMap();
-	final Key tempKey = new Key();
+
+	public AnimationState (AnimationStateData data) {
+		this.data = data;
+	}
 
 	public void apply (Skeleton skeleton) {
 		if (current == null) return;
@@ -55,16 +57,6 @@ public class AnimationState {
 		mixTime += delta;
 	}
 
-	/** Set the mixing duration between two animations. */
-	public void setMixing (Animation from, Animation to, float duration) {
-		if (from == null) throw new IllegalArgumentException("from cannot be null.");
-		if (to == null) throw new IllegalArgumentException("to cannot be null.");
-		Key key = new Key();
-		key.a1 = from;
-		key.a2 = to;
-		animationToMixTime.put(key, duration);
-	}
-
 	/** Set the current animation. */
 	public void setAnimation (Animation animation, boolean loop) {
 		setAnimation(animation, loop, 0);
@@ -75,10 +67,8 @@ public class AnimationState {
 	public void setAnimation (Animation animation, boolean loop, float time) {
 		previous = null;
 		if (animation != null && current != null) {
-			tempKey.a1 = current;
-			tempKey.a2 = animation;
-			if (animationToMixTime.containsKey(tempKey)) {
-				mixDuration = animationToMixTime.get(tempKey);
+			mixDuration = data.getMixing(current, animation);
+			if (mixDuration > 0) {
 				mixTime = 0;
 				previous = current;
 			}
@@ -98,24 +88,7 @@ public class AnimationState {
 		return currentTime;
 	}
 
-	static class Key {
-		Animation a1, a2;
-
-		public int hashCode () {
-			return 31 * (31 + a1.hashCode()) + a2.hashCode();
-		}
-
-		public boolean equals (Object obj) {
-			if (this == obj) return true;
-			if (obj == null) return false;
-			Key other = (Key)obj;
-			if (a1 == null) {
-				if (other.a1 != null) return false;
-			} else if (!a1.equals(other.a1)) return false;
-			if (a2 == null) {
-				if (other.a2 != null) return false;
-			} else if (!a2.equals(other.a2)) return false;
-			return true;
-		}
+	public AnimationStateData getData () {
+		return data;
 	}
 }
---
+++ spine-libgdx/src/com/esotericsoftware/spine/AnimationStateData.java
@@ -0,0 +1,73 @@
+/*******************************************************************************
+ * Copyright (c) 2013, Esoteric Software
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ ******************************************************************************/
+
+package com.esotericsoftware.spine;
+
+import com.badlogic.gdx.utils.ObjectMap;
+
+/** Stores mixing times between animations. */
+public class AnimationStateData {
+	final ObjectMap<Key, Float> animationToMixTime = new ObjectMap();
+	final Key tempKey = new Key();
+
+	/** Set the mixing duration between two animations. */
+	public void setMixing (Animation from, Animation to, float duration) {
+		if (from == null) throw new IllegalArgumentException("from cannot be null.");
+		if (to == null) throw new IllegalArgumentException("to cannot be null.");
+		Key key = new Key();
+		key.a1 = from;
+		key.a2 = to;
+		animationToMixTime.put(key, duration);
+	}
+
+	public float getMixing (Animation from, Animation to) {
+		tempKey.a1 = from;
+		tempKey.a2 = to;
+		Float time = animationToMixTime.get(tempKey);
+		if (time == null) return 0;
+		return time;
+	}
+
+	static class Key {
+		Animation a1, a2;
+
+		public int hashCode () {
+			return 31 * (31 + a1.hashCode()) + a2.hashCode();
+		}
+
+		public boolean equals (Object obj) {
+			if (this == obj) return true;
+			if (obj == null) return false;
+			Key other = (Key)obj;
+			if (a1 == null) {
+				if (other.a1 != null) return false;
+			} else if (!a1.equals(other.a1)) return false;
+			if (a2 == null) {
+				if (other.a2 != null) return false;
+			} else if (!a2.equals(other.a2)) return false;
+			return true;
+		}
+	}
+}
