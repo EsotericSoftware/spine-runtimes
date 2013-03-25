ff62c847f2ca93e28bb494d0374d82e7d60c476e
Stefan Nguyen <stefan@vinova.sg>
2013-03-25 21:58:37 +0700

    fix proj.win32

--- spine-cocos2dx/example/proj.win32/main.cpp
+++ spine-cocos2dx/example/proj.win32/main.cpp
@@ -1,11 +1,23 @@
 #include "main.h"
-#include "../example/AppDelegate.h"
+#include "../Classes/AppDelegate.h"
+#include "CCEGLView.h"
 
 USING_NS_CC;
 
-int APIENTRY _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nCmdShow) {
-	UNREFERENCED_PARAMETER(hPrevInstance);
-	UNREFERENCED_PARAMETER(lpCmdLine);
-	AppDelegate app;
-	return CCApplication::sharedApplication()->run();
+int APIENTRY _tWinMain(HINSTANCE hInstance,
+                       HINSTANCE hPrevInstance,
+                       LPTSTR    lpCmdLine,
+                       int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // create the application instance
+    AppDelegate app;
+    CCEGLView* eglView = CCEGLView::sharedOpenGLView();
+    eglView->setViewName("ExampleSpine");
+    eglView->setFrameSize(480, 320);
+    // So we need to invoke 'setFrameZoomFactor'(only valid on desktop(win32, mac, linux)) to make the window smaller.
+   // eglView->setFrameZoomFactor(0.4f);
+    return CCApplication::sharedApplication()->run();
 }
