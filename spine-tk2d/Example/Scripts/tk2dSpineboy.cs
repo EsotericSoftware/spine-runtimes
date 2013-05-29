5070b21341f0d7a14b85e19768f4affda780e34a
NathanSweet <nathan.sweet@gmail.com>
2013-05-29 03:24:44 +0200

    Initialize skeleton on start.

--- spine-tk2d/Example/Scripts/tk2dSpineboy.cs
+++ spine-tk2d/Example/Scripts/tk2dSpineboy.cs
@@ -18,9 +18,9 @@ public class tk2dSpineboy : MonoBehaviour {
 	/*
 	 */
 	void Update() {
-		if(skeleton.loop) return;
+		if (skeleton.loop) return;
 		
-		if(skeleton.state.Time >= skeleton.state.Animation.Duration - 0.25) {
+		if (skeleton.state.Animation != null && skeleton.state.Time >= skeleton.state.Animation.Duration - 0.25) {
 			skeleton.animationName = "walk";
 			skeleton.loop = true;
 		}
