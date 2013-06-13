dcbd87d90a2e1747cfc9e1738c01c1a123e1cd36
NathanSweet <nathan.sweet@gmail.com>
2013-06-13 20:00:25 +0200

    Added defaultMix to AnimationStateData.

--- spine-js/spine.js
+++ spine-js/spine.js
@@ -750,6 +750,7 @@ spine.AnimationStateData = function (skeletonData) {
 	this.animationToMixTime = {};
 };
 spine.AnimationStateData.prototype = {
+	defaultMix: 0,
 	setMixByName: function (fromName, toName, duration) {
 		var from = this.skeletonData.findAnimation(fromName);
 		if (!from) throw "Animation not found: " + fromName;
@@ -762,7 +763,7 @@ spine.AnimationStateData.prototype = {
 	},
 	getMix: function (from, to) {
 		var time = this.animationToMixTime[from.name + ":" + to.name];
-		return time ? time : 0;
+		return time ? time : this.defaultMix;
 	}
 };
 
