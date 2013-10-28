6b20ba4e9718072085e4c39e8eb7d6c629dd372d
NathanSweet <nathan.sweet@gmail.com>
2013-10-28 20:43:45 +0100

    Removed stray f character.
    closes #145

--- spine-js/spine.js
+++ spine-js/spine.js
@@ -541,7 +541,7 @@ spine.EventTimeline.prototype = {
 
 		if (lastTime > time) { // Fire events after last time for looped animations.
 			apply(skeleton, lastTime, Number.MAX_VALUE, firedEvents, alpha);
-			lastTime = -1f;
+			lastTime = -1;
 		} else if (lastTime >= frames[frameCount - 1]) // Last time is after last frame.
 			return;
 		if (time < frames[0]) return; // Time is before first frame.
