654df6ac64bc30d9db7f85495f8866986e36a243
NathanSweet <nathan.sweet@gmail.com>
2013-10-31 14:00:54 +0100

    Unused import.

--- spine-libgdx/test/com/esotericsoftware/spine/NormalMapTest.java
+++ spine-libgdx/test/com/esotericsoftware/spine/NormalMapTest.java
@@ -37,7 +37,6 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
