4fa11e72d5c30b50583f4399dbffaaa9dcf57213
NathanSweet <nathan.sweet@gmail.com>
2014-03-18 19:02:11 +0100

    Fixed spine-unity and spine-tk2d to create assets in the current folder.

--- spine-unity/Assets/Spine/Editor/Menus.cs
+++ spine-unity/Assets/Spine/Editor/Menus.cs
@@ -43,14 +43,15 @@ public class SpineEditor {
 		CreateAsset<SkeletonDataAsset>("New SkeletonData");
 	}
 	
-	static private void CreateAsset <T> (String path) where T : ScriptableObject {
-		try {
-			path = Path.GetDirectoryName(AssetDatabase.GetAssetPath(Selection.activeObject)) + "/" + path;
-		} catch (Exception) {
-			path = "Assets/" + path;
+	static private void CreateAsset <T> (String name) where T : ScriptableObject {
+		var dir = "Assets/";
+		var selected = Selection.activeObject;
+		if (selected != null) {
+			var assetDir = AssetDatabase.GetAssetPath(selected.GetInstanceID());
+			if (assetDir.Length > 0 && Directory.Exists(assetDir)) dir = assetDir + "/";
 		}
 		ScriptableObject asset = ScriptableObject.CreateInstance<T>();
-		AssetDatabase.CreateAsset(asset, path + ".asset");
+		AssetDatabase.CreateAsset(asset, dir + name + ".asset");
 		AssetDatabase.SaveAssets();
 		EditorUtility.FocusProjectWindow();
 		Selection.activeObject = asset;
