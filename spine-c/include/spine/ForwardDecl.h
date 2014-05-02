7df7fccb34b314755321f3daac2f370e853c9df9
jpoag <James.Poag@gmail.com>
2014-05-02 08:26:50 -0400

    * ForwardDecl.h usage notes

--- spine-c/include/spine/ForwardDecl.h
+++ spine-c/include/spine/ForwardDecl.h
@@ -4,30 +4,36 @@
 /*
  Notes:
  
-	- Forward declares reduce build times, especially for large game projects
-	- Also prevents polluting by not requiring types that you don't need.
-	
-	- Cannot forward declare enumerated types, consider using 'int' and (c-style) casting
-	  or moving all the enums to this file and using it as universal header for spine
-	
-	- Other forward declare types can be added, just follow example of spAtlas
-	
+    - Forward declares reduce build times, especially for large game projects
+    - Also prevents polluting by not requiring types that you don't need.
+    
+    - Cannot forward declare enumerated types, consider using 'int' and (c-style) casting
+      or moving all the enums to this file and using it as universal header for spine
+    
+    - Other forward declare types can be added, just follow example of spAtlas
+    
+Usage:
+
+    - #include "spine/ForwardDecl.h" in the header files for your game that uses Spine data type
+      pointers* and references&
+      
+    - #include "spine/ " relevant headers in the cpp files that actually use the Spine objects
  */
  
 extern "C"{
 
-	typedef struct spEvent spEvent;
-	
-	typedef struct spAtlas spAtlas;
-	
-	typedef struct spAnimationState spAnimationState;
-	typedef struct spAnimationStateData spAnimationStateData;
-	
-	typedef struct spSkeleton spSkeleton;
-	typedef struct spSkeletonBounds spSkeletonBounds;
-	typedef struct spSkeletonData spSkeletonData;
-	typedef struct spSlot spSlot;
-	typedef struct spAttachment spAttachment;
+    typedef struct spEvent spEvent;
+    
+    typedef struct spAtlas spAtlas;
+    
+    typedef struct spAnimationState spAnimationState;
+    typedef struct spAnimationStateData spAnimationStateData;
+    
+    typedef struct spSkeleton spSkeleton;
+    typedef struct spSkeletonBounds spSkeletonBounds;
+    typedef struct spSkeletonData spSkeletonData;
+    typedef struct spSlot spSlot;
+    typedef struct spAttachment spAttachment;
 };
 
 #endif /* SPINE_FORWARDDECL_H_ */
