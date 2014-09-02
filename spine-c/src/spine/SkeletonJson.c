ecab41ab8b025d2172c6c2e22aa0301f2cefcbe4
NathanSweet <nathan.sweet@gmail.com>
2014-09-02 13:02:15 +0200

    Fixed bend direction for IK keys.

--- spine-c/src/spine/SkeletonJson.c
+++ spine-c/src/spine/SkeletonJson.c
@@ -235,7 +235,7 @@ static spAnimation* _spSkeletonJson_readAnimation (spSkeletonJson* self, Json* r
 		}
 		for (frame = ikMap->child, i = 0; frame; frame = frame->next, ++i) {
 			spIkConstraintTimeline_setFrame(timeline, i, Json_getFloat(frame, "time", 0), Json_getFloat(frame, "mix", 0),
-					Json_getFloat(frame, "bendPositive", 1) ? 1 : -1);
+					Json_getInt(frame, "bendPositive", 1) ? 1 : -1);
 			readCurve(SUPER(timeline), i, frame);
 		}
 		animation->timelines[animation->timelinesCount++] = SUPER_CAST(spTimeline, timeline);
