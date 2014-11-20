cad68f30ff61ee6d2ec1f3539fb5d9b148093af4
NathanSweet <nathan.sweet@gmail.com>
2014-11-20 12:42:54 +0100

    Organized spine-libgdx so tests and the skeleton viewer are separate.

--- spine-libgdx/spine-libgdx/src/com/esotericsoftware/spine/AnimationState.java
+++ spine-libgdx/spine-libgdx/src/com/esotericsoftware/spine/AnimationState.java
@@ -304,7 +304,7 @@ public class AnimationState {
 		TrackEntry next, previous;
 		Animation animation;
 		boolean loop;
-		float delay, time, lastTime = -1, endTime, timeScale = 1;
+		float delay, time, lastTime = 0, endTime, timeScale = 1;
 		float mixTime, mixDuration;
 		AnimationStateListener listener;
 		float mix = 1;
