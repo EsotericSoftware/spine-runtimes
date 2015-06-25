5ee7823116b56feec65af9a62ffe3d4cacbea309
Fenrisul <mitch@xdtech.net>
2015-06-24 20:24:24 -0700

    [Unity] Spine Gauge Example and misc importer tweak

--- spine-unity/Assets/spine-unity/Editor/SpineEditorUtilities.cs
+++ spine-unity/Assets/spine-unity/Editor/SpineEditorUtilities.cs
@@ -727,6 +727,7 @@ public class SpineEditorUtilities : AssetPostprocessor {
 			Texture2D texture = (Texture2D)AssetDatabase.LoadAssetAtPath(texturePath, typeof(Texture2D));
 
 			TextureImporter texImporter = (TextureImporter)TextureImporter.GetAtPath(texturePath);
+			texImporter.textureType = TextureImporterType.Advanced;
 			texImporter.textureFormat = TextureImporterFormat.AutomaticTruecolor;
 			texImporter.mipmapEnabled = false;
 			texImporter.alphaIsTransparency = false;
