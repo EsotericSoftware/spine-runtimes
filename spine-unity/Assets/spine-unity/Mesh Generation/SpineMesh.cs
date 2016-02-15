200570ea0e2e0ed0b61d06fa661ec055697cef79
pharan <johndygit@live.com.ph>
2016-02-15 18:16:48 +0800

    NewMesh should be marked dynamic.

--- spine-unity/Assets/spine-unity/Mesh Generation/SpineMesh.cs
+++ spine-unity/Assets/spine-unity/Mesh Generation/SpineMesh.cs
@@ -4,8 +4,10 @@ using System.Collections;
 public static class SpineMesh {
 	internal const HideFlags MeshHideflags = HideFlags.DontSaveInBuild | HideFlags.DontSaveInEditor;
 
+	/// <summary>Factory method for creating a new mesh for use in Spine components. This can be called in field initializers.</summary>
 	public static Mesh NewMesh () {
 		var m = new Mesh();
+		m.MarkDynamic();
 		m.name = "Skeleton Mesh";
 		m.hideFlags = SpineMesh.MeshHideflags;
 		return m;
