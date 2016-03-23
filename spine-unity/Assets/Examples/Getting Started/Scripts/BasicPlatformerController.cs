129a92afece6072fa62d6f7e8650c08ca8d1eb41
pharan <johndygit@live.com.ph>
2016-03-24 01:58:10 +0800

    Updated sample scenes.

--- spine-unity/Assets/Examples/Getting Started/Scripts/BasicPlatformerController.cs
+++ spine-unity/Assets/Examples/Getting Started/Scripts/BasicPlatformerController.cs
@@ -7,6 +7,7 @@
 
 using UnityEngine;
 using System.Collections;
+using Spine.Unity;
 
 [RequireComponent(typeof(CharacterController))]
 public class BasicPlatformerController : MonoBehaviour {
--- spine-unity/Assets/Examples/Getting Started/Scripts/SpineBeginnerTwo.cs
+++ spine-unity/Assets/Examples/Getting Started/Scripts/SpineBeginnerTwo.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Spine.Unity;
 
 public class SpineBeginnerTwo : MonoBehaviour {
 
