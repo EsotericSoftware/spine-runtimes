3da9f835010bf33b8c7187845129c7e745b003f6
NathanSweet <nathan.sweet@gmail.com>
2016-03-31 14:07:43 +0200

    Fixed region scale.

--- spine-libgdx/spine-libgdx/src/com/esotericsoftware/spine/SkeletonBinary.java
+++ spine-libgdx/spine-libgdx/src/com/esotericsoftware/spine/SkeletonBinary.java
@@ -276,8 +276,8 @@ public class SkeletonBinary {
 			region.setScaleX(scaleX);
 			region.setScaleY(scaleY);
 			region.setRotation(rotation);
-			region.setWidth(width);
-			region.setHeight(height);
+			region.setWidth(width * scale);
+			region.setHeight(height * scale);
 			Color.rgba8888ToColor(region.getColor(), color);
 			region.updateOffset();
 			return region;
@@ -390,7 +390,6 @@ public class SkeletonBinary {
 				mesh.setWidth(width * scale);
 				mesh.setHeight(height * scale);
 			}
-			// BOZO - Store to look up source attachment later.
 			return mesh;
 		}
 		case weightedlinkedmesh: {
