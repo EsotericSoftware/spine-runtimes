422e9f4e54468d65d18935f2211be10da42a73a1
Pavel Platto <hinidu@gmail.com>
2016-06-30 16:39:14 +0200

    [spine-c] fix spMeshAttachment_updateUVs (use worldVerticesLength instead of verticesCount).

--- spine-c/src/spine/MeshAttachment.c
+++ spine-c/src/spine/MeshAttachment.c
@@ -59,15 +59,16 @@ spMeshAttachment* spMeshAttachment_create (const char* name) {
 void spMeshAttachment_updateUVs (spMeshAttachment* self) {
 	int i;
 	float width = self->regionU2 - self->regionU, height = self->regionV2 - self->regionV;
+	int verticesLength = SUPER(self)->worldVerticesLength;
 	FREE(self->uvs);
-	self->uvs = MALLOC(float, self->super.verticesCount);
+	self->uvs = MALLOC(float, verticesLength);
 	if (self->regionRotate) {
-		for (i = 0; i < self->super.verticesCount; i += 2) {
+		for (i = 0; i < verticesLength; i += 2) {
 			self->uvs[i] = self->regionU + self->regionUVs[i + 1] * width;
 			self->uvs[i + 1] = self->regionV + height - self->regionUVs[i] * height;
 		}
 	} else {
-		for (i = 0; i < self->super.verticesCount; i += 2) {
+		for (i = 0; i < verticesLength; i += 2) {
 			self->uvs[i] = self->regionU + self->regionUVs[i] * width;
 			self->uvs[i + 1] = self->regionV + self->regionUVs[i + 1] * height;
 		}
