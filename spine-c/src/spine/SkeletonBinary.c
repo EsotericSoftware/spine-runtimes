0762f3ccf2f4092bc206f9e5253112bbfe3d8c65
yanweicai <yanweicai@moqikaka.com>
2016-08-30 16:04:17 +0800

    fix bug when using SkeletonBinary.

--- spine-c/src/spine/SkeletonBinary.c
+++ spine-c/src/spine/SkeletonBinary.c
@@ -716,6 +716,9 @@ spAttachment* spSkeletonBinary_readAttachment(spSkeletonBinary* self, _dataInput
 				mesh->width = 0;
 				mesh->height = 0;
 			}
+
+            spAttachmentLoader_configureAttachment(self->attachmentLoader, attachment);
+
 			return attachment;
 		}
 		case SP_ATTACHMENT_LINKED_MESH: {
