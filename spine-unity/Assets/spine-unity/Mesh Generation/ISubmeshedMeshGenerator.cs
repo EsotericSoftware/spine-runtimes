fbd8e32c062f98a7f1c9d055463067a0a9503460
pharan <johndygit@live.com.ph>
2016-09-08 20:43:03 +0800

    [unity] Fix PMA for SkeletonRenderSeparator.

--- spine-unity/Assets/spine-unity/Mesh Generation/ISubmeshedMeshGenerator.cs
+++ spine-unity/Assets/spine-unity/Mesh Generation/ISubmeshedMeshGenerator.cs
@@ -28,6 +28,7 @@ namespace Spine.Unity.MeshGeneration {
 		MeshAndMaterials GenerateMesh (ExposedList<SubmeshInstruction> instructions, int startSubmesh, int endSubmesh);
 
 		float ZSpacing { get; set; }
+		bool PremultiplyVertexColors { get; set; }
 		bool AddNormals { get; set; }
 		bool AddTangents { get; set; }
 	}
