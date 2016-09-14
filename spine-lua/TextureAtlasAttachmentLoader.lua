9feb9fcd61e25fbf7ff2d49a19912c9af1bde59f
badlogic <badlogicgames@gmail.com>
2016-09-14 17:04:15 +0200

    [lua] Added TextureAtlasAttachmentLoader

---
+++ spine-lua/TextureAtlasAttachmentLoader.lua
@@ -0,0 +1,74 @@
+-------------------------------------------------------------------------------
+-- Spine Runtimes Software License
+-- Version 2.3
+-- 
+-- Copyright (c) 2013-2015, Esoteric Software
+-- All rights reserved.
+-- 
+-- You are granted a perpetual, non-exclusive, non-sublicensable and
+-- non-transferable license to use, install, execute and perform the Spine
+-- Runtimes Software (the "Software") and derivative works solely for personal
+-- or internal use. Without the written permission of Esoteric Software (see
+-- Section 2 of the Spine Software License Agreement), you may not (a) modify,
+-- translate, adapt or otherwise create derivative works, improvements of the
+-- Software or develop new applications using the Software or (b) remove,
+-- delete, alter or obscure any trademarks or any copyright, trademark, patent
+-- or other intellectual property or proprietary rights notices on or in the
+-- Software, including any copy thereof. Redistributions in binary or source
+-- form must include this license and terms.
+-- 
+-- THIS SOFTWARE IS PROVIDED BY ESOTERIC SOFTWARE "AS IS" AND ANY EXPRESS OR
+-- IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+-- MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+-- EVENT SHALL ESOTERIC SOFTWARE BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+-- SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+-- PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+-- OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+-- WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+-- OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+-- ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+-------------------------------------------------------------------------------
+local setmetatable = setmetatable
+
+local AttachmentType = require "spine-lua.attachments.AttachmentType"
+local RegionAttachment = require "spine-lua.attachments.RegionAttachment"
+local BoundingBoxAttachment = require "spine-lua.attachments.BoundingBoxAttachment"
+local MeshAttachment = require "spine-lua.attachments.MeshAttachment"
+local SkinningMeshAttachment = require "spine-lua.attachments.SkinnedMeshAttachment"
+local TextureAtlas = require "spine-lua.TextureAtlas"
+
+local TextureAtlasAttachmentLoader = {}
+TextureAtlasAttachmentLoader.__index = TextureAtlasAttachmentLoader
+
+function TextureAtlasAttachmentLoader.new (atlas)
+	local self = {
+    atlas = atlas
+  }
+  setmetatable(self, TextureAtlasAttachmentLoader)
+	return self
+end
+
+function TextureAtlasAttachmentLoader:newRegionAttachment (skin, name, path)
+  local region = self.atlas:findRegion(path)
+  if not region then error("Region not found in atlas: " .. path .. " (region attachment: " .. name .. ")") end
+  region.renderObject = region
+  local attachment = RegionAttachment.new(name)
+  attachment:setRegion(region)
+  attachment.region = region
+  return attachment
+end
+
+function TextureAtlasAttachmentLoader:newMeshAttachment (skin, name, path)
+  -- FIXME atlas regions
+  return MeshAttachment.new(name)
+end
+
+function TextureAtlasAttachmentLoader:newSkinningMeshAttachment (skin, name, path)
+  return SkinningMeshAttachment.new(name)
+end
+
+function TextureAtlasAttachmentLoader:newBoundingBoxAttachment (skin, name)
+  return BoundingBoxAttachment.new(name)
+end
+
+return TextureAtlasAttachmentLoader
