a1a38b59b2ed1dd4be3f9bbf6e2fabee6cbec986
badlogic <badlogicgames@gmail.com>
2016-10-10 14:08:43 +0200

    [corona] Initial version of new Corona renderer based on meshes. Kind of works, except that the parent group x/y aren't taken into account

--- spine-corona/main.lua
+++ spine-corona/main.lua
@@ -1,6 +1,155 @@
-require "examples.spineboy.spineboy"
+require("mobdebug").start()
+
+-- require "examples.spineboy.spineboy"
 -- require "examples.spineboy-atlas.spineboy"
 -- require "examples.spineboy.spineboy-mesh"
 -- require "examples.goblins.goblins"
 -- require "examples.dragon.dragon"
 -- require "examples.hero.hero"
+
+local spine = require "spine-corona.spine-corona"
+
+local QUAD_TRIANGLES = { 1, 2, 3, 3, 4, 1 }
+spine.Skeleton.new_super = spine.Skeleton.new
+spine.Skeleton.updateWorldTransform_super = spine.Skeleton.updateWorldTransform
+spine.Skeleton.new = function(skeletonData, group)
+  self = spine.Skeleton.new_super(skeletonData)
+  self.parentGroup = group or display.newGroup()
+  self.drawingGroup = display.newGroup()
+  self.parentGroup:insert(self.drawingGroup)
+  self.premultipliedAlpha = false
+  
+  return self
+end
+
+function spine.Skeleton:updateWorldTransform()
+  spine.Skeleton.updateWorldTransform_super(self)
+  local premultipliedAlpha = self.premultipliedAlpha
+  
+  -- Remove old drawing group, we will start anew
+  self.drawingGroup:removeSelf()
+  local drawingGroup = display.newGroup()
+  self.drawingGroup = drawingGroup
+  self.drawingGroup.parent = self.parentGroup
+  
+  local drawOrder = self.drawOrder
+  local currentGroup = nil
+  local groupVertices = {}
+  local groupIndices = {}
+  local groupUvs = {}
+  local texture = nil
+  local lastTexture = nil
+  for i,slot in ipairs(drawOrder) do
+    local attachment = slot.attachment
+    local vertices = nil
+    local indices = nil
+    if attachment then
+      if attachment.type == spine.AttachmentType.region then
+        vertices = attachment:updateWorldVertices(slot, premultipliedAlpha)
+        indices = QUAD_TRIANGLES
+        texture = attachment.region.renderObject.texture
+      elseif attachment.type == spine.AttachmentType.mesh then
+        vertices = attachment:updateWorldVertices(slot, premultipliedAlpha)
+        indices = attachment.triangles
+        texture = attachment.region.renderObject.texture
+      end
+      
+      if texture then
+        if texture ~= lastTexture and lastTexture then -- FIXME need to take color and blend mode into account
+          self:flush(groupVertices, groupUvs, groupIndices, texture, drawingGroup)
+          lastTexture = texture
+          groupVertices = {}
+          groupUvs = {}
+          groupIndices = {}
+        end
+      
+        self:batch(vertices, indices, groupVertices, groupUvs, groupIndices)
+      end
+    end
+  end
+  
+  if #groupVertices > 0 then
+    self:flush(groupVertices, groupUvs, groupIndices, texture, drawingGroup)
+  end
+end
+
+function spine.Skeleton:flush(groupVertices, groupUvs, groupIndices, texture, drawingGroup)
+  mesh = display.newMesh(drawingGroup, 0, 0, {
+      mode = "indexed",
+      vertices = groupVertices,
+      uvs = groupUvs,
+      indices = groupIndices
+  })
+  mesh.fill = texture
+  mesh:translate(mesh.path:getVertexOffset())
+end
+
+function spine.Skeleton:batch(vertices, indices, groupVertices, groupUvs, groupIndices)
+  local numIndices = #indices
+  local i = 1
+  local indexStart = #groupIndices + 1
+  local offset = #groupVertices / 2
+  local indexEnd = indexStart + numIndices
+
+  while indexStart < indexEnd do
+    groupIndices[indexStart] = indices[i] + offset
+    indexStart = indexStart + 1
+    i = i + 1
+  end
+  
+  i = 1
+  local numVertices = #vertices
+  local vertexStart = #groupVertices + 1
+  local vertexEnd = vertexStart + numVertices / 4
+  while vertexStart < vertexEnd do
+    groupVertices[vertexStart] = vertices[i]
+    groupVertices[vertexStart+1] = vertices[i+1]
+    groupUvs[vertexStart] = vertices[i+2]
+    groupUvs[vertexStart+1] = vertices[i+3]
+    vertexStart = vertexStart + 2
+    i = i + 8
+    -- FIXME color
+  end
+end
+
+local loader = function (path) 
+  local paint = { type = "image", filename = "data/" .. path }
+  return paint
+end
+local atlas = spine.TextureAtlas.new(spine.utils.readFile("data/spineboy.atlas"), loader)
+local json = spine.SkeletonJson.new(spine.TextureAtlasAttachmentLoader.new(atlas))
+json.scale = 0.3
+local skeletonData = json:readSkeletonDataFile("data/spineboy.json")
+
+local skeletonGroup = display.newGroup()
+skeletonGroup.name = "muhhh"
+skeletonGroup.y = 100
+local skeleton = spine.Skeleton.new(skeletonData, skeletonGroup)
+skeleton.flipY = true
+local animationStateData = spine.AnimationStateData.new(skeletonData)
+local animation = spine.AnimationState.new(animationStateData)
+animation:setAnimationByName(0, "walk", true)
+
+local img = display.newImage("data/spineboy.png")
+img.width = 100
+img.height = 100
+
+display.setDefault("background", 0.2, 0.2, 0.2, 1)
+
+local lastTime = 0
+Runtime:addEventListener("enterFrame", function (event)        
+	local currentTime = event.time / 1000
+	local delta = currentTime - lastTime
+	lastTime = currentTime
+  
+  animation:update(delta)
+  animation:apply(skeleton)
+  skeleton:updateWorldTransform()
+end)
+    
+Runtime:addEventListener("touch", function(event)
+    skeletonGroup.x = event.x
+    skeletonGroup.y = event.y
+    img.x = event.x
+    img.y = event.y
+end)
---
+++ spine-corona/spine-corona/spine-corona.lua
@@ -0,0 +1,81 @@
+-------------------------------------------------------------------------------
+-- Spine Runtimes Software License
+-- Version 2.3
+-- 
+-- Copyright (c) 2013-2015, Esoteric Software
+-- All rights reserved.
+-- 
+-- You are granted a perpetual, non-exclusive, non-sublicensable and
+-- non-transferable license to use, install, execute and perform the Spine
+-- Runtimes Software (the "Software") and derivative works solely for personal
+-- or internal use. Without the written permission of Esoteric Software (see
+-- Section 2 of the Spine Software License Agreement), you may not (a) modify,
+-- translate, adapt or otherwise create derivative works, improvements of the
+-- Software or develop new applications using the Software or (b) remove,
+-- delete, alter or obscure any trademarks or any copyright, trademark, patent
+-- or other intellectual property or proprietary rights notices on or in the
+-- Software, including any copy thereof. Redistributions in binary or source
+-- form must include this license and terms.
+-- 
+-- THIS SOFTWARE IS PROVIDED BY ESOTERIC SOFTWARE "AS IS" AND ANY EXPRESS OR
+-- IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+-- MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+-- EVENT SHALL ESOTERIC SOFTWARE BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+-- SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+-- PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+-- OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+-- WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+-- OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+-- ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+-------------------------------------------------------------------------------
+
+spine = {}
+
+spine.utils = require "spine-lua.utils"
+spine.SkeletonJson = require "spine-lua.SkeletonJson"
+spine.SkeletonData = require "spine-lua.SkeletonData"
+spine.BoneData = require "spine-lua.BoneData"
+spine.SlotData = require "spine-lua.SlotData"
+spine.IkConstraintData = require "spine-lua.IkConstraintData"
+spine.Skin = require "spine-lua.Skin"
+spine.Attachment = require "spine-lua.attachments.Attachment"
+spine.BoundingBoxAttachment = require "spine-lua.attachments.BoundingBoxAttachment"
+spine.RegionAttachment = require "spine-lua.attachments.RegionAttachment"
+spine.MeshAttachment = require "spine-lua.attachments.MeshAttachment"
+spine.VertexAttachment = require "spine-lua.attachments.VertexAttachment"
+spine.PathAttachment = require "spine-lua.attachments.PathAttachment"
+spine.Skeleton = require "spine-lua.Skeleton"
+spine.Bone = require "spine-lua.Bone"
+spine.Slot = require "spine-lua.Slot"
+spine.IkConstraint = require "spine-lua.IkConstraint"
+spine.AttachmentType = require "spine-lua.attachments.AttachmentType"
+spine.AttachmentLoader = require "spine-lua.AttachmentLoader"
+spine.Animation = require "spine-lua.Animation"
+spine.AnimationStateData = require "spine-lua.AnimationStateData"
+spine.AnimationState = require "spine-lua.AnimationState"
+spine.EventData = require "spine-lua.EventData"
+spine.Event = require "spine-lua.Event"
+spine.SkeletonBounds = require "spine-lua.SkeletonBounds"
+spine.BlendMode = require "spine-lua.BlendMode"
+spine.TextureAtlas = require "spine-lua.TextureAtlas"
+spine.TextureRegion = require "spine-lua.TextureRegion"
+spine.TextureAtlasRegion = require "spine-lua.TextureAtlasRegion"
+spine.TextureAtlasAttachmentLoader = require "spine-lua.TextureAtlasAttachmentLoader"
+spine.Color = require "spine-lua.Color"
+
+spine.utils.readFile = function (fileName, base)
+	if not base then base = system.ResourceDirectory end
+	local path = system.pathForFile(fileName, base)
+	local file = io.open(path, "r")
+	if not file then return nil end
+	local contents = file:read("*a")
+	io.close(file)
+	return contents
+end
+ 
+local json = require "json"
+spine.utils.readJSON = function (text)
+	return json.decode(text)
+end
+
+return spine
