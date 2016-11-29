25eae31ae826f04c0cb13b6554592221f52ebc65
NathanSweet <nathan.sweet@gmail.com>
2016-11-29 19:07:28 +0100

    [libgdx] Less ugly test code.

--- spine-libgdx/spine-skeletonviewer/src/com/esotericsoftware/spine/SkeletonViewer.java
+++ spine-libgdx/spine-skeletonviewer/src/com/esotericsoftware/spine/SkeletonViewer.java
@@ -218,15 +218,6 @@ public class SkeletonViewer extends ApplicationAdapter {
 		setAnimation();
 
 		// ui.animationList.clearListeners();
-		// state.addListener(new AnimationState.AnimationStateAdapter() {
-		// public void complete (TrackEntry entry) {
-		// System.out.println("complete " + entry + ", " + entry.hashCode());
-		// System.out.println("complete " + entry + ", " + entry.hashCode());
-		// state.setAnimation(0, Math.random() > 0.5? "walk" : "jump", false);
-		// state.update(0);
-		// state.apply(skeleton);
-		// }
-		// });
 		// state.setAnimation(0, "walk", true);
 	}
 
