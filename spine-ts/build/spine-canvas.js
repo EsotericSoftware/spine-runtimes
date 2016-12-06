ac48da3a148680ac6f5c1367c716c7708c1cbdc3
badlogic <badlogicgames@gmail.com>
2016-12-06 13:43:44 +0100

    [ts] Ported latest AnimationState changes, see c512ee5d595076842cbb12d95a3a7f06792b81d1

--- spine-ts/build/spine-canvas.js
+++ spine-ts/build/spine-canvas.js
@@ -1398,6 +1398,7 @@ var spine;
 					}
 				}
 				this.queueEvents(current, animationTime);
+				events.length = 0;
 				current.nextAnimationLast = animationTime;
 				current.nextTrackLast = current.trackTime;
 			}
@@ -1441,7 +1442,9 @@ var spine;
 					timeline.apply(skeleton, animationLast, animationTime, events, alpha, setupPose, true);
 				}
 			}
-			this.queueEvents(from, animationTime);
+			if (entry.mixDuration > 0)
+				this.queueEvents(from, animationTime);
+			this.events.length = 0;
 			from.nextAnimationLast = animationTime;
 			from.nextTrackLast = from.trackTime;
 			return mix;
@@ -1529,7 +1532,6 @@ var spine;
 					continue;
 				this.queue.event(entry, events[i]);
 			}
-			this.events.length = 0;
 		};
 		AnimationState.prototype.clearTracks = function () {
 			this.queue.drainDisabled = true;
