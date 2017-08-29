e02f8b7a1cdb4634d13859e7b00037b5230311a4
badlogic <badlogicgames@gmail.com>
2017-08-29 11:37:19 +0200

    [ue4] Updated to 4.17, fixed build system, fixed c++ example. Closes

--- spine-ue4/Plugins/RuntimeMeshComponentPlugin/Source/RuntimeMeshComponent/RuntimeMeshComponent.Build.cs
+++ spine-ue4/Plugins/RuntimeMeshComponentPlugin/Source/RuntimeMeshComponent/RuntimeMeshComponent.Build.cs
@@ -4,7 +4,7 @@ using UnrealBuildTool;
 
 public class RuntimeMeshComponent : ModuleRules
 {
-	public RuntimeMeshComponent(TargetInfo Target)
+	public RuntimeMeshComponent(ReadOnlyTargetRules Target) : base(Target)
 	{
         PrivateIncludePaths.Add("RuntimeMeshComponent/Private");
         PublicIncludePaths.Add("RuntimeMeshComponent/Public");
