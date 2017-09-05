61b9e76cfe0f5cab5d73723710f9fedfff78ffe4
badlogic <badlogicgames@gmail.com>
2017-09-05 11:56:40 +0200

    [ue4] Cleaned up MySceneComponent

--- spine-ue4/Source/SpineUE4/MySceneComponent.cpp
+++ spine-ue4/Source/SpineUE4/MySceneComponent.cpp
@@ -11,9 +11,6 @@ UMySceneComponent::UMySceneComponent(const FObjectInitializer& ObjectInitializer
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
-
-	// ...
-	spColor_create();
 }
 
 
