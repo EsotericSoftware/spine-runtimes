d67ac7120ccdd2f260b9e7f5b44a3c5de33b7262
Stephen Gowen <dev.sgowen@gmail.com>
2017-09-29 18:25:00 -0400

    wip

---
+++ spine-cpp/spine-cpp/src/spine/IkConstraintData.cpp
@@ -0,0 +1,101 @@
+/******************************************************************************
+* Spine Runtimes Software License v2.5
+*
+* Copyright (c) 2013-2016, Esoteric Software
+* All rights reserved.
+*
+* You are granted a perpetual, non-exclusive, non-sublicensable, and
+* non-transferable license to use, install, execute, and perform the Spine
+* Runtimes software and derivative works solely for personal or internal
+* use. Without the written permission of Esoteric Software (see Section 2 of
+* the Spine Software License Agreement), you may not (a) modify, translate,
+* adapt, or develop new applications using the Spine Runtimes or otherwise
+* create derivative works or improvements of the Spine Runtimes or (b) remove,
+* delete, alter, or obscure any trademarks or any copyright, trademark, patent,
+* or other intellectual property or proprietary rights notices on or in the
+* Software, including any copy thereof. Redistributions in binary or source
+* form must include this license and terms.
+*
+* THIS SOFTWARE IS PROVIDED BY ESOTERIC SOFTWARE "AS IS" AND ANY EXPRESS OR
+* IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+* MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+* EVENT SHALL ESOTERIC SOFTWARE BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+* SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+* PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES, BUSINESS INTERRUPTION, OR LOSS OF
+* USE, DATA, OR PROFITS) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+* IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+* ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+* POSSIBILITY OF SUCH DAMAGE.
+*****************************************************************************/
+
+#include <spine/IkConstraintData.h>
+
+namespace Spine
+{
+    IkConstraintData::IkConstraintData(std::string name) :
+    _name(name),
+    _order(0),
+    _target(nullptr),
+    _bendDirection(1),
+    _mix(1)
+    {
+        // Empty
+    }
+    
+    const std::string& IkConstraintData::getName()
+    {
+        return _name;
+    }
+    
+    int IkConstraintData::getOrder()
+    {
+        return _order;
+    }
+    
+    void IkConstraintData::setOrder(int inValue)
+    {
+        _order = inValue;
+    }
+    
+    std::list<BoneData*>& IkConstraintData::getBones()
+    {
+        return _bones;
+    }
+    
+    BoneData* IkConstraintData::getTarget()
+    {
+        return _target;
+    }
+    
+    void IkConstraintData::setTarget(BoneData* inValue)
+    {
+        _target = inValue;
+    }
+    
+    int IkConstraintData::getBendDirection()
+    {
+        return _bendDirection;
+    }
+    
+    void IkConstraintData::setBendDirection(int inValue)
+    {
+        _bendDirection = inValue;
+    }
+    
+    float IkConstraintData::getMix()
+    {
+        return _mix;
+    }
+    
+    void IkConstraintData::setMix(float inValue)
+    {
+        _mix = inValue;
+    }
+    
+    std::ostream& operator <<(std::ostream& os, const IkConstraintData& ref)
+    {
+        os << ref._name;
+        
+        return os;
+    }
+}
