5205d68d780b25a62c59eb2e785ead7a421f79c1
halx99 <halx99@live.com>
2017-10-15 21:55:01 +0800

    Update SkeletonDataCache.cpp

--- spine-cocos2dx/src/spine/SkeletonDataCache.cpp
+++ spine-cocos2dx/src/spine/SkeletonDataCache.cpp
@@ -4,132 +4,132 @@ static auto error_log = &cocos2d::log;
 
 namespace spine {
 
-void SkeletonDataCache::setErrorLogFunc(void(*errorfunc)(const char* pszFormat, ...))
-{
+    void SkeletonDataCache::setErrorLogFunc(void(*errorfunc)(const char* pszFormat, ...))
+    {
         error_log = errorfunc;
-}
+    }
 
-SkeletonDataCache* SkeletonDataCache::getInstance()
-{
-	static SkeletonDataCache internalShared;
-	return &internalShared;
-}
+    SkeletonDataCache* SkeletonDataCache::getInstance()
+    {
+        static SkeletonDataCache internalShared;
+        return &internalShared;
+    }
 
-void SkeletonDataCache::removeData(const char* dataFile)
-{
-	auto target = _cacheTable.find(dataFile);
-	if (target != _cacheTable.end()) {
-		target->second->release();
-		_cacheTable.erase(target);
-	}
-}
+    void SkeletonDataCache::removeData(const char* dataFile)
+    {
+        auto target = _cacheTable.find(dataFile);
+        if (target != _cacheTable.end()) {
+            target->second->release();
+            _cacheTable.erase(target);
+        }
+    }
 
-SkeletonDataCache::SkeletonData* SkeletonDataCache::addData(const char* dataFile, const char* atlasFile, float scale)
-{
-	auto target = _cacheTable.find(dataFile);
-	if (target != _cacheTable.end()) {
-		return target->second;
-	}
+    SkeletonDataCache::SkeletonData* SkeletonDataCache::addData(const char* dataFile, const char* atlasFile, float scale)
+    {
+        auto target = _cacheTable.find(dataFile);
+        if (target != _cacheTable.end()) {
+            return target->second;
+        }
 
-	spSkeletonData* skeletonData = nullptr;
-	spAttachmentLoader* loader = nullptr;
-	bool ok = false;
+        spSkeletonData* skeletonData = nullptr;
+        spAttachmentLoader* loader = nullptr;
+        bool ok = false;
 
-    auto fileExtension = cocos2d::FileUtils::getInstance()->getFileExtension(dataFile);
+        auto fileExtension = cocos2d::FileUtils::getInstance()->getFileExtension(dataFile);
 
-	do {
-		spAtlas* atlas = spAtlas_createFromFile(atlasFile, 0);
+        do {
+            spAtlas* atlas = spAtlas_createFromFile(atlasFile, 0);
 
-		if (nullptr == (atlas))
-			break;
+            if (nullptr == (atlas))
+                break;
 
-		loader = (spAttachmentLoader*)Cocos2dAttachmentLoader_create(atlas);
+            loader = (spAttachmentLoader*)Cocos2dAttachmentLoader_create(atlas);
 
-        int failed = 0;
+            int failed = 0;
 
-        /*
-        ** Atlas is used by shared attachment loader, temp json or binary should be dispose.
-        ** Cache, we just need SkeletonData & atlas.
-        */
-        if (fileExtension == ".skel") {
-            auto binary = spSkeletonBinary_createWithLoader(loader);
-            if (nullptr == binary) {
+            /*
+            ** Atlas is used by shared attachment loader, temp json or binary should be dispose.
+            ** Cache, we just need SkeletonData & atlas.
+            */
+            if (fileExtension == ".skel") {
+                auto binary = spSkeletonBinary_createWithLoader(loader);
+                if (nullptr == binary) {
+                    spAtlas_dispose(atlas);
+                    break;
+                }
+
+                binary->scale = scale;
+                skeletonData = spSkeletonBinary_readSkeletonDataFile(binary, dataFile);
+                if ((binary->error != nullptr)) {
+                    ++failed;
+                    error_log("#parse spine .skel data file failed, error:%s", binary->error);
+                }
+
+                spSkeletonBinary_dispose(binary);
+            }
+            else {
+                spSkeletonJson* json = spSkeletonJson_createWithLoader(loader);
+                if (nullptr == json) {
+                    spAtlas_dispose(atlas);
+                    break;
+                }
+
+                json->scale = scale;
+                skeletonData = spSkeletonJson_readSkeletonDataFile(json, dataFile);
+                if ((json->error != nullptr)) {
+                    ++failed;
+                    error_log("#parse spine .json data file failed, error:%s", json->error);
+                }
+
+                spSkeletonJson_dispose(json);
+            }
+
+            if ((loader->error1 != nullptr)) {
+                ++failed;
+                error_log("#parse spine attachment failed, error:%s%s", loader->error1, loader->error2);
+            }
+
+            if (failed > 0) {
+                if (skeletonData != nullptr)
+                    spSkeletonData_dispose(skeletonData);
+
                 spAtlas_dispose(atlas);
+                spAttachmentLoader_dispose(loader);
                 break;
             }
 
-            binary->scale = scale;
-            skeletonData = spSkeletonBinary_readSkeletonDataFile(binary, dataFile);
-            if ((binary->error != nullptr)) {
-                ++failed;
-                error_log("#parse spine .skel data file failed, error:%s", binary->error);
-            }
+            ok = true;
+        } while (false);
 
-            spSkeletonBinary_dispose(binary);
+        if (ok) {
+            auto newData = new SkeletonData(skeletonData, loader);
+            _cacheTable.emplace(dataFile, newData);
+            return newData;
         }
-        else {
-            spSkeletonJson* json = spSkeletonJson_createWithLoader(loader); 
-            if (nullptr == json) {
-                spAtlas_dispose(atlas);
-                break;
-            }
+        return nullptr;
+    }
 
-            json->scale = scale;
-            skeletonData = spSkeletonJson_readSkeletonDataFile(json, dataFile);
-            if ((json->error != nullptr)) {
-                ++failed;
-                error_log("#parse spine .json data file failed, error:%s", json->error);
-            }
-
-            spSkeletonJson_dispose(json);
+    void SkeletonDataCache::removeAllData(void)
+    {
+        for (auto & e : _cacheTable)
+        {
+            e.second->release();
         }
+        _cacheTable.clear();
+    }
 
-		if ((loader->error1 != nullptr)) {
-			++failed;
-			error_log("#parse spine attachment failed, error:%s%s", loader->error1, loader->error2);
-		}
-		
-		if (failed > 0) {
-			if (skeletonData != nullptr)
-				spSkeletonData_dispose(skeletonData);
-
-			spAtlas_dispose(atlas);
-			spAttachmentLoader_dispose(loader);
-			break;
-		}
-
-		ok = true;
-	} while (false);
-
-	if (ok) {
-		auto newData = new SkeletonData(skeletonData, loader);
-		_cacheTable.emplace(dataFile, newData);
-		return newData;
-	}
-	return nullptr;
-}
-
-void SkeletonDataCache::removeAllData(void)
-{
-	for (auto & e : _cacheTable)
-	{
-		e.second->release();
-	}
-	_cacheTable.clear();
-}
-
-void SkeletonDataCache::removeAllUnusedData(void)
-{
-	auto _First = _cacheTable.begin();
-	auto _Last = _cacheTable.end();
-	for (; _First != _Last; ) {
-		if ((*_First).second->getReferenceCount() == 1) {
-			(*_First).second->release();
-			_cacheTable.erase(_First++);
-			continue;
-		}
-		++_First;
-	}
-}
+    void SkeletonDataCache::removeAllUnusedData(void)
+    {
+        auto _First = _cacheTable.begin();
+        auto _Last = _cacheTable.end();
+        for (; _First != _Last; ) {
+            if ((*_First).second->getReferenceCount() == 1) {
+                (*_First).second->release();
+                _cacheTable.erase(_First++);
+                continue;
+            }
+            ++_First;
+        }
+    }
 
 };
