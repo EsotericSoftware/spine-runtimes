0a61f34c61aa604ddc7a9da06266c84ca28ebe99
pharan <johndygit@live.com.ph>
2018-04-04 17:17:06 +0800

    [unity] Updated examples.

---
+++ spine-unity/Assets/Spine Examples/Scripts/Mix and Match Character Customize/EquipAssetExample.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Spine.Unity.Examples {
+	[CreateAssetMenu]
+	public class EquipAssetExample : ScriptableObject {
+		public EquipSystemExample.EquipType equipType;
+		public Sprite sprite;
+		public string description;
+		public int yourStats;
+	}
+
+}
