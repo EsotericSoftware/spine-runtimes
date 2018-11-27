7175cf98e71fad4e739e5c092b3afdf0da26ea2e
badlogic <badlogicgames@gmail.com>
2018-11-27 12:24:15 +0100

    [c][cpp] Closes #1213, don't trim end of atlas file line.

--- spine-cpp/spine-cpp/src/spine/Atlas.cpp
+++ spine-cpp/spine-cpp/src/spine/Atlas.cpp
@@ -249,9 +249,9 @@ void Atlas::trim(Str *str) {
 
 	str->end--;
 
-	while (isspace((unsigned char) *str->end) && str->end >= str->begin) {
+	/*while (isspace((unsigned char) *str->end) && str->end >= str->begin) {
 		str->end--;
-	}
+	}*/
 
 	str->end++;
 }
