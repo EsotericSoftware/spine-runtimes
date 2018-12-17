075d4a1be5e81adacd521a8b48a1df77f13d7203
badlogic <badlogicgames@gmail.com>
2018-12-17 15:32:25 +0100

    [ts][player] Removed old widget. Renamed TSC output to spine-player.js, moved sources from widget/ to player/.

--- spine-ts/build.sh
+++ spine-ts/build.sh
@@ -5,5 +5,5 @@ tsc -p tsconfig.core.json
 tsc -p tsconfig.webgl.json
 tsc -p tsconfig.canvas.json
 tsc -p tsconfig.threejs.json
-tsc -p tsconfig.widget.json
+tsc -p tsconfig.player.json
 ls build/*.js build/*.ts | awk '{print "unexpand -t 4 ", $0, " > /tmp/e; mv /tmp/e ", $0}' | sh
