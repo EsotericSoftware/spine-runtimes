0180f593368a196a53403f2b79bb65966a527c7b
pharan <johndygit@live.com.ph>
2018-12-20 10:54:19 +0800

    [unity] Improved Mecanim+SkeletonAnimation example.

---
+++ spine-unity/Assets/Spine Examples/Scripts/Mecanim as Logic/MecanimToAnimationHandleExample.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Spine.Unity.Examples {
+	
+	// This StateMachineBehaviour handles sending the Mecanim state information to the component that handles playing the Spine animations.
+	public class MecanimToAnimationHandleExample : StateMachineBehaviour {
+		SkeletonAnimationHandleExample animationHandle;
+		bool initialized;
+
+		override public void OnStateEnter (Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+			if (!initialized) {
+				animationHandle = animator.GetComponent<SkeletonAnimationHandleExample>();
+				initialized = true;
+			}
+
+			animationHandle.PlayAnimationForState(stateInfo.shortNameHash, layerIndex);
+		}
+	}
+
+}
