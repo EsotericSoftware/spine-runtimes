0aa617c44a3662d97ce514673af6e1600410e446
pharan <johndygit@live.com.ph>
2018-12-27 21:49:19 +0800

    [unity] Updated some example scenes, code and assets.

--- spine-unity/Assets/Spine Examples/Scripts/Mecanim as Logic/SkeletonAnimationHandleExample.cs
+++ spine-unity/Assets/Spine Examples/Scripts/Mecanim as Logic/SkeletonAnimationHandleExample.cs
@@ -1,14 +1,47 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿/******************************************************************************
+ * Spine Runtimes Software License v2.5
+ *
+ * Copyright (c) 2013-2016, Esoteric Software
+ * All rights reserved.
+ *
+ * You are granted a perpetual, non-exclusive, non-sublicensable, and
+ * non-transferable license to use, install, execute, and perform the Spine
+ * Runtimes software and derivative works solely for personal or internal
+ * use. Without the written permission of Esoteric Software (see Section 2 of
+ * the Spine Software License Agreement), you may not (a) modify, translate,
+ * adapt, or develop new applications using the Spine Runtimes or otherwise
+ * create derivative works or improvements of the Spine Runtimes or (b) remove,
+ * delete, alter, or obscure any trademarks or any copyright, trademark, patent,
+ * or other intellectual property or proprietary rights notices on or in the
+ * Software, including any copy thereof. Redistributions in binary or source
+ * form must include this license and terms.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ESOTERIC SOFTWARE "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+ * EVENT SHALL ESOTERIC SOFTWARE BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES, BUSINESS INTERRUPTION, OR LOSS OF
+ * USE, DATA, OR PROFITS) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *****************************************************************************/
+
+ using System.Collections.Generic;
 using UnityEngine;
 
-
 namespace Spine.Unity.Examples {
-	public class SkeletonAnimationHandleExample : MonoBehaviour {
 
+	// This is an example of an animation handle. This is implemented with strings as state names.
+	// Strings can serve as the identifier when Mecanim is used as the state machine and state source.
+	// If you don't use Mecanim, using custom ScriptableObjects may be a more efficient way to store information about the state and its connection with specific Spine animations.
+
+	// This animation handle implementation also comes with a dummy implementation of transition-handling.
+	public class SkeletonAnimationHandleExample : MonoBehaviour {
 		public SkeletonAnimation skeletonAnimation;
 		public List<StateNameToAnimationReference> statesAndAnimations = new List<StateNameToAnimationReference>();
-		public List<AnimationTransition> transitions = new List<AnimationTransition>();
+		public List<AnimationTransition> transitions = new List<AnimationTransition>(); // Alternately, an AnimationPair-Animation Dictionary (commented out) can be used for more efficient lookups.
 
 		[System.Serializable]
 		public class StateNameToAnimationReference {
@@ -17,31 +50,46 @@ namespace Spine.Unity.Examples {
 		}
 
 		[System.Serializable]
-		public struct AnimationTransition {
+		public class AnimationTransition {
 			public AnimationReferenceAsset from;
 			public AnimationReferenceAsset to;
 			public AnimationReferenceAsset transition;
 		}
 
-		readonly Dictionary<Spine.AnimationStateData.AnimationPair, Spine.Animation> transitionDictionary = new Dictionary<AnimationStateData.AnimationPair, Animation>(Spine.AnimationStateData.AnimationPairComparer.Instance);
+		//readonly Dictionary<Spine.AnimationStateData.AnimationPair, Spine.Animation> transitionDictionary = new Dictionary<AnimationStateData.AnimationPair, Animation>(Spine.AnimationStateData.AnimationPairComparer.Instance);
+
+		public Spine.Animation TargetAnimation { get; private set; }
 
 		void Awake () {
+			// Initialize AnimationReferenceAssets
+			foreach (var entry in statesAndAnimations) {
+				entry.animation.Initialize();
+			}
 			foreach (var entry in transitions) {
-				// If uninitialized
 				entry.from.Initialize();
 				entry.to.Initialize();
 				entry.transition.Initialize();
-
-				transitionDictionary.Add(new AnimationStateData.AnimationPair(entry.from.Animation, entry.to.Animation), entry.transition.Animation);
 			}
+
+			// Build Dictionary
+			//foreach (var entry in transitions) {
+			//	transitionDictionary.Add(new AnimationStateData.AnimationPair(entry.from.Animation, entry.to.Animation), entry.transition.Animation);
+			//}
 		}
 
+		/// <summary>Sets the horizontal flip state of the skeleton based on a nonzero float. If negative, the skeleton is flipped. If positive, the skeleton is not flipped.</summary>
 		public void SetFlip (float horizontal) {
 			if (horizontal != 0) {
 				skeletonAnimation.Skeleton.ScaleX = horizontal > 0 ? 1f : -1f;
 			}
 		}
 
+		/// <summary>Plays an animation based on the state name.</summary>
+		public void PlayAnimationForState (string stateShortName, int layerIndex) {
+			PlayAnimationForState(StringToHash(stateShortName), layerIndex);
+		}
+
+		/// <summary>Plays an  animation based on the hash of the state name.</summary>
 		public void PlayAnimationForState (int shortNameHash, int layerIndex) {
 			var foundAnimation = GetAnimationForState(shortNameHash);
 			if (foundAnimation == null)
@@ -50,21 +98,25 @@ namespace Spine.Unity.Examples {
 			PlayNewAnimation(foundAnimation, layerIndex);
 		}
 
+		/// <summary>Gets a Spine Animation based on the state name.</summary>
+		public Spine.Animation GetAnimationForState (string stateShortName) {
+			return GetAnimationForState(StringToHash(stateShortName));
+		}
+
+		/// <summary>Gets a Spine Animation based on the hash of the state name.</summary>
 		public Spine.Animation GetAnimationForState (int shortNameHash) {
-			var foundState = statesAndAnimations.Find(entry => Animator.StringToHash(entry.stateName) == shortNameHash);
+			var foundState = statesAndAnimations.Find(entry => StringToHash(entry.stateName) == shortNameHash);
 			return (foundState == null) ? null : foundState.animation;
 		}
 
-		void PlayNewAnimation (Spine.Animation target, int layerIndex) {
+		/// <summary>Play an animation. If a transition animation is defined, the transition is played before the target animation being passed.</summary>
+		public void PlayNewAnimation (Spine.Animation target, int layerIndex) {
 			Spine.Animation transition = null;
 			Spine.Animation current = null;
 
-			var currentTrackEntry = skeletonAnimation.AnimationState.GetCurrent(layerIndex);
-			if (currentTrackEntry != null) {
-				current = currentTrackEntry.Animation;
-				if (current != null)
-					transitionDictionary.TryGetValue(new AnimationStateData.AnimationPair(current, target), out transition);
-			}
+			current = GetCurrentAnimation(layerIndex);
+			if (current != null)
+				transition = TryGetTransition(current, target);
 
 			if (transition != null) {
 				skeletonAnimation.AnimationState.SetAnimation(layerIndex, transition, false);
@@ -72,6 +124,42 @@ namespace Spine.Unity.Examples {
 			} else {
 				skeletonAnimation.AnimationState.SetAnimation(layerIndex, target, true);
 			}
+
+			this.TargetAnimation = target;
+		}
+
+		/// <summary>Play a non-looping animation once then continue playing the state animation.</summary>
+		public void PlayOneShot (Spine.Animation oneShot, int layerIndex) {
+			var state = skeletonAnimation.AnimationState;
+			state.SetAnimation(0, oneShot, false);
+
+			var transition = TryGetTransition(oneShot, TargetAnimation);
+			if (transition != null)
+				state.AddAnimation(0, transition, false, 0f);
+
+			state.AddAnimation(0, this.TargetAnimation, true, 0f);
+		}
+
+		Spine.Animation TryGetTransition (Spine.Animation from, Spine.Animation to) {			
+			foreach (var transition in transitions) {
+				if (transition.from.Animation == from && transition.to.Animation == to) {
+					return transition.transition.Animation;
+				}
+			}
+			return null;
+
+			//Spine.Animation foundTransition = null;
+			//transitionDictionary.TryGetValue(new AnimationStateData.AnimationPair(from, to), out foundTransition);
+			//return foundTransition;
+		}
+
+		Spine.Animation GetCurrentAnimation (int layerIndex) {
+			var currentTrackEntry = skeletonAnimation.AnimationState.GetCurrent(layerIndex);
+			return (currentTrackEntry != null) ? currentTrackEntry.Animation : null;
+		}
+
+		int StringToHash (string s) {
+			return Animator.StringToHash(s);
 		}
 	}
 }
