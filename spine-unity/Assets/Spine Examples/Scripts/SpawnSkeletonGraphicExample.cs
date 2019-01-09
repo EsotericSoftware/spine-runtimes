3da52c46b35f9c40ff6060b834aa93427ae99d7f
Harald Csaszar <harald.csaszar@pixelcloudgames.com>
2019-01-09 16:18:15 +0100

    [unity] Fixed Instantiate from Script example (missing script in SkeletonGraphic part), closes #294

---
+++ spine-unity/Assets/Spine Examples/Scripts/SpawnSkeletonGraphicExample.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Spine.Unity.Examples {
+	public class SpawnSkeletonGraphicExample : MonoBehaviour {
+
+		public SkeletonDataAsset skeletonDataAsset;
+
+		[SpineAnimation(dataField: "skeletonDataAsset")]
+		public string startingAnimation;
+
+		[SpineSkin(dataField: "skeletonDataAsset")]
+		public string startingSkin = "base";
+		public Material skeletonGraphicMaterial;
+		
+		IEnumerator Start () {
+			if (skeletonDataAsset == null) yield break;
+			skeletonDataAsset.GetSkeletonData(false); // Preload SkeletonDataAsset.
+			yield return new WaitForSeconds(1f); // Pretend stuff is happening.
+
+			var sg = SkeletonGraphic.NewSkeletonGraphicGameObject(skeletonDataAsset, this.transform, skeletonGraphicMaterial); // Spawn a new SkeletonGraphic GameObject.
+			sg.gameObject.name = "SkeletonGraphic Instance";
+
+			// Extra Stuff
+			sg.Initialize(false);
+			sg.Skeleton.SetSkin(startingSkin);
+			sg.Skeleton.SetSlotsToSetupPose();
+			sg.AnimationState.SetAnimation(0, startingAnimation, true);
+		}
+	}
+
+}
