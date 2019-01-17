3c06a0881f136428b878dafb78ae6f14235fc6c4
badlogic <badlogicgames@gmail.com>
2019-01-17 16:35:03 +0100

    [ue4] Clean-up of assets and build scripts.

--- spine-ue4/Plugins/SpinePlugin/Source/SpinePlugin/SpinePlugin.Build.cs
+++ spine-ue4/Plugins/SpinePlugin/Source/SpinePlugin/SpinePlugin.Build.cs
@@ -7,7 +7,9 @@ namespace UnrealBuildTool.Rules
 	{
 		public SpinePlugin(ReadOnlyTargetRules Target) : base(Target)
 		{
-			PublicIncludePaths.Add(Path.Combine(ModuleDirectory, "Public"));
+            PrivatePCHHeaderFile = "Private/SpinePluginPrivatePCH.h";
+
+            PublicIncludePaths.Add(Path.Combine(ModuleDirectory, "Public"));
 			PublicIncludePaths.Add(Path.Combine(ModuleDirectory, "Public/spine-cpp/include"));
 
 			PrivateIncludePaths.Add(Path.Combine(ModuleDirectory, "Private"));
@@ -15,10 +17,6 @@ namespace UnrealBuildTool.Rules
 
             PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "RHI", "RenderCore", "ShaderCore", "ProceduralMeshComponent", "UMG", "Slate", "SlateCore" });
 			PublicDefinitions.Add("SPINE_UE4");
-
-			// For UE 4.19 and below comment the line above and uncomment the line
-			// below.
-			// Definitions.Add("SPINE_UE4");
 		}
 	}
 }
--- spine-ue4/Source/SpineUE4/SpineUE4.Build.cs
+++ spine-ue4/Source/SpineUE4/SpineUE4.Build.cs
@@ -1,21 +1,11 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 using UnrealBuildTool;
 
 public class SpineUE4 : ModuleRules
 {
 	public SpineUE4(ReadOnlyTargetRules Target) : base(Target)
 	{
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "SpinePlugin", "ProceduralMeshComponent" });
-
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
-
-		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
-
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+        PrivatePCHHeaderFile = "SpineUE4.h";
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "SpinePlugin", "ProceduralMeshComponent" });
+		PrivateDependencyModuleNames.AddRange(new string[] {  });		
 	}
 }
