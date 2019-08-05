aa660443c684159e3781013a2e4ae33b07fedd89
Predator75 <cristian.zerbinati@gmail.com>
2019-08-05 11:05:32 +0200

    FNA Runtime
    FNA is a reimplementation of the Microsoft XNA Game Studio 4.0 Refresh libraries.
    FNA is primarily developed by video game porter Ethan Lee, who has shipped more than three dozen ports of XNA games using the exact branch that you see on GitHub today.
    it can run on win,mac,linux and same console, it supports x86/x64
    https://fna-xna.github.io/
    
    FNA is fully supported by Steam.
    
    I converted the spine xna runtimes to make them work with fna, it is very easy.
    
    I attach an archive with fna runtime
    and I link a fully working test project: https://www.eprime.it/download/spine_test_con_sprite.zip (sorry we work in vbnet ;) )

---
+++ spine-fna/spine-fna/src/Util.cs
@@ -0,0 +1,80 @@
+/******************************************************************************
+ * Spine Runtimes License Agreement
+ * Last updated May 1, 2019. Replaces all prior versions.
+ *
+ * Copyright (c) 2013-2019, Esoteric Software LLC
+ *
+ * Integration of the Spine Runtimes into software or otherwise creating
+ * derivative works of the Spine Runtimes is permitted under the terms and
+ * conditions of Section 2 of the Spine Editor License Agreement:
+ * http://esotericsoftware.com/spine-editor-license
+ *
+ * Otherwise, it is permitted to integrate the Spine Runtimes into software
+ * or otherwise create derivative works of the Spine Runtimes (collectively,
+ * "Products"), provided that each user of the Products must obtain their own
+ * Spine Editor license and redistribution of the Products in any form must
+ * include this license and copyright notice.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ESOTERIC SOFTWARE LLC "AS IS" AND ANY EXPRESS
+ * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN
+ * NO EVENT SHALL ESOTERIC SOFTWARE LLC BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES, BUSINESS
+ * INTERRUPTION, OR LOSS OF USE, DATA, OR PROFITS) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *****************************************************************************/
+
+using System;
+using System.IO;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+#if WINDOWS_STOREAPP
+using System.Threading.Tasks;
+using Windows.Storage;
+#endif
+
+namespace Spine {
+
+	static public class Util {
+#if WINDOWS_STOREAPP
+		private static async Task<Texture2D> LoadFile(GraphicsDevice device, String path) {
+			var folder = Windows.ApplicationModel.Package.Current.InstalledLocation;
+			var file = await folder.GetFileAsync(path).AsTask().ConfigureAwait(false);
+			try {
+				return Util.LoadTexture(device, await file.OpenStreamForReadAsync().ConfigureAwait(false));
+			} catch (Exception ex) {
+				throw new Exception("Error reading texture file: " + path, ex);
+			}
+		}
+
+		static public Texture2D LoadTexture (GraphicsDevice device, String path) {
+			return LoadFile(device, path).Result;
+		}
+#else
+		static public Texture2D LoadTexture (GraphicsDevice device, String path) {
+
+#if WINDOWS_PHONE
+            Stream stream = Microsoft.Xna.Framework.TitleContainer.OpenStream(path);
+            using (Stream input = stream)
+            {
+#else
+            using (Stream input = new FileStream(path, FileMode.Open, FileAccess.Read)) {
+#endif
+				try {
+					return Util.LoadTexture(device, input);
+				} catch (Exception ex) {
+					throw new Exception("Error reading texture file: " + path, ex);
+				}
+			}
+		}
+#endif
+
+		static public Texture2D LoadTexture (GraphicsDevice device, Stream input) {
+			return Texture2D.FromStream(device, input);
+		}
+	}
+}
