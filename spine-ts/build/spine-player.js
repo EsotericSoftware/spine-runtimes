4e96ebcda2d471cffe9ce69539bdbb5d7244854a
badlogic <badlogicgames@gmail.com>
2019-10-31 17:10:22 +0100

    [ts][webgl] Added . When  it will disable the corresponding WebGL pixel store behaviour.

--- spine-ts/build/spine-player.js
+++ spine-ts/build/spine-player.js
@@ -8253,6 +8253,8 @@ var spine;
 					this.texture = this.context.gl.createTexture();
 				}
 				this.bind();
+				if (GLTexture.DISABLE_UNPACK_PREMULTIPLIED_ALPHA_WEBGL)
+					gl.pixelStorei(gl.UNPACK_PREMULTIPLY_ALPHA_WEBGL, false);
 				gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, this._image);
 				gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
 				gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, useMipMaps ? gl.LINEAR_MIPMAP_LINEAR : gl.LINEAR);
@@ -8282,6 +8284,7 @@ var spine;
 				var gl = this.context.gl;
 				gl.deleteTexture(this.texture);
 			};
+			GLTexture.DISABLE_UNPACK_PREMULTIPLIED_ALPHA_WEBGL = false;
 			return GLTexture;
 		}(spine.Texture));
 		webgl.GLTexture = GLTexture;
