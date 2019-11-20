572560d313980b11fa88315f009f48982f271082
Harald Csaszar <harald.csaszar@pixelcloudgames.com>
2019-11-20 14:29:35 +0100

    [unity] Updated Hero skeleton assets to new combined one with sword and morning star skins. Added example scene "SkeletonUtility Platformer HingeChain Physics" for demonstration of hinge chain physics.

---
+++ spine-unity/Assets/Spine Examples/Scripts/Sample Components/SetRigidbodySolverIterations.cs
@@ -0,0 +1,54 @@
+﻿/******************************************************************************
+ * Spine Runtimes License Agreement
+ * Last updated May 1, 2019. Replaces all prior versions.
+ *
+ * Copyright (c) 2013-2019, Esoteric Software LLC
+ *
+ * Integration of the Spine Runtimes into software or otherwise creating
+ * derivative works of the Spine Runtimes is permitted under the terms and
+ * conditions of Section 2 of the Spine Editor License Agreement:
+ * http://esotericsoftware.com/spine-editor-license
+ *
+ * Otherwise, it is permitted to integrate the Spine Runtimes into software
+ * or otherwise create derivative works of the Spine Runtimes (collectively,
+ * "Products"), provided that each user of the Products must obtain their own
+ * Spine Editor license and redistribution of the Products in any form must
+ * include this license and copyright notice.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ESOTERIC SOFTWARE LLC "AS IS" AND ANY EXPRESS
+ * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN
+ * NO EVENT SHALL ESOTERIC SOFTWARE LLC BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES, BUSINESS
+ * INTERRUPTION, OR LOSS OF USE, DATA, OR PROFITS) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *****************************************************************************/
+
+using UnityEngine;
+
+namespace Spine.Unity.Examples {
+
+	/// <summary>
+	/// This component is intended to increase the physics solver iteration count
+	/// for Rigidbody Joint setups which would otherwise be too unstable.
+	///
+	/// To use this example component, add it to a GameObject which is parent of
+	/// one or more Rigidbody instances. The physics setting "solver iteration count"
+	/// will be overwritten by the provided value.
+	/// </summary>
+	[DisallowMultipleComponent]
+	public class SetRigidbodySolverIterations : MonoBehaviour
+	{
+		public int solverIterations = 30;
+
+		void Awake () {
+			var rigidbodies = this.GetComponentsInChildren<Rigidbody>();
+			foreach (var rb in rigidbodies) {
+				rb.solverIterations = solverIterations;
+			}
+		}
+	}
+}
