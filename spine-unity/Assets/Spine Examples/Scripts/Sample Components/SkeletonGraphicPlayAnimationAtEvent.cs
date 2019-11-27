b3e29c4338827f7a4443381cc73e285422422d8e
Harald Csaszar <harald.csaszar@pixelcloudgames.com>
2019-11-27 13:46:59 +0100

    [unity] Added `Outline Shaders` example scene to demonstrate switching to outline materials. See #1531.

---
+++ spine-unity/Assets/Spine Examples/Scripts/Sample Components/SkeletonGraphicPlayAnimationAtEvent.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using Spine.Unity;
+
+public class SkeletonGraphicPlayAnimationAtEvent : MonoBehaviour {
+
+	public SkeletonGraphic skeletonGraphic;
+	public int trackIndex = 0;
+	public float playbackSpeed = 1.0f;
+
+	public void PlayAnimationLooping (string animation) {
+		var entry = skeletonGraphic.AnimationState.SetAnimation(trackIndex, animation, true);
+		entry.TimeScale = playbackSpeed;
+	}
+
+	public void PlayAnimationOnce (string animation) {
+		var entry = skeletonGraphic.AnimationState.SetAnimation(trackIndex, animation, false);
+		entry.TimeScale = playbackSpeed;
+	}
+
+	public void ClearTrack () {
+		skeletonGraphic.AnimationState.ClearTrack(trackIndex);
+	}
+}
