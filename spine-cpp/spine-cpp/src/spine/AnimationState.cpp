b73cd19d12630c378d4ad542aa77e444e1bf5718
yinjimmy <jimmy.yin5@gmail.com>
2020-02-18 09:40:28 +0700

    fix: can not clear track when it's end time is reached

--- spine-cpp/spine-cpp/src/spine/AnimationState.cpp
+++ spine-cpp/spine-cpp/src/spine/AnimationState.cpp
@@ -932,7 +932,7 @@ TrackEntry *AnimationState::newTrackEntry(size_t trackIndex, Animation *animatio
 	entry._trackTime = 0;
 	entry._trackLast = -1;
 	entry._nextTrackLast = -1; // nextTrackLast == -1 signifies a TrackEntry that wasn't applied yet.
-	entry._trackEnd = std::numeric_limits<float>::max(); // loop ? float.MaxValue : animation.Duration;
+	entry._trackEnd = loop ? std::numeric_limits<float>::max() : animation->getDuration();
 	entry._timeScale = 1;
 
 	entry._alpha = 1;
