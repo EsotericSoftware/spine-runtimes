c73f8c1f42e7bb57d6c23cfe400fe0001155c844
Harald Csaszar <harald.csaszar@pixelcloudgames.com>
2020-05-06 16:03:04 +0200

    [csharp] Ported automated test set 'AnimationStateTests' to csharp and Unity.

---
+++ spine-unity/Assets/SpineTests/src/RunAnimationStateTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Spine.Unity.Tests
+{
+    public class RunAnimationStateTests
+    {
+        [Test]
+        public void RunAnimationStateTestsSimplePasses ()
+        {
+			AnimationStateTests.logImplementation += Log;
+			AnimationStateTests.failImplementation += Fail;
+			AnimationStateTests.Main("Assets/SpineTests/spine-csharp-tests/tests/assets/test.json");
+        }
+
+		public void Log (string message) {
+			UnityEngine.Debug.Log(message);
+		}
+
+		public void Fail (string message) {
+			Assert.Fail(message);
+		}
+	}
+}
