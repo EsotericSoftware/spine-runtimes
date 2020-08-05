a3f90f7d65f87bf40391bd3c8f6b1bf05114837a
HALX99 <halx99@live.com>
2020-08-05 17:01:04 +0800

    Update spine-cocos2dx.cpp

--- spine-cocos2dx/src/spine/spine-cocos2dx.cpp
+++ spine-cocos2dx/src/spine/spine-cocos2dx.cpp
@@ -167,7 +167,7 @@ Cocos2dExtension::Cocos2dExtension() : DefaultSpineExtension() { }
 Cocos2dExtension::~Cocos2dExtension() { }
 
 char *Cocos2dExtension::_readFile(const spine::String &path, int *length) {
-	Data data = FileUtils::getInstance()->getDataFromFile(FileUtils::getInstance()->fullPathForFilename(path.buffer()));
+	Data data = FileUtils::getInstance()->getDataFromFile(path.buffer());
 	if (data.isNull()) return nullptr;
 
 	// avoid buffer overflow (int is shorter than ssize_t in certain platforms)
