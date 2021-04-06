97b3f3058b08ebd48034694659c64a14aba3a41f
Harald Csaszar <harald.csaszar@pixelcloudgames.com>
2021-04-06 17:44:50 +0200

    [unity] Added `RootMotionDeltaCompensation` example component demonstrating simple root motion delta compensation. See #1873.

---
+++ spine-unity/Assets/Spine Examples/Scripts/Sample Components/RootMotionDeltaCompensation.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using Spine.Unity;
+
+namespace Spine.Unity.Examples {
+
+	public class RootMotionDeltaCompensation : MonoBehaviour {
+
+		protected SkeletonRootMotionBase rootMotion;
+		public Transform targetPosition;
+		public int trackIndex = 0;
+
+		void Start () {
+			rootMotion = this.GetComponent<SkeletonRootMotionBase>();
+		}
+
+		void Update () {
+			AdjustDelta();
+		}
+
+		void OnDisable () {
+			rootMotion.rootMotionScaleX = rootMotion.rootMotionScaleY = 1;
+		}
+
+		void AdjustDelta() {
+			Vector3 toTarget = targetPosition.position - this.transform.position;
+			rootMotion.AdjustRootMotionToDistance(toTarget, trackIndex);
+		}
+	}
+}
