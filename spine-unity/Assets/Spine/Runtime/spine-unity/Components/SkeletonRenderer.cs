EsotericSoftware/spine-runtimes

commit 5cb54bdf9cc7903b28d834762a2bcaac38e559ce

[unity] Fixed UpdateMode when invisible not being used when instantiated off-screen. Closes #1883.

spine-unity/Assets/Spine/Runtime/spine-unity/Components/SkeletonRenderer.cs
@@ -280,6 +280,7 @@ namespace Spine.Unity {
 
 		public virtual void Awake () {
 			Initialize(false);
+			updateMode = updateWhenInvisible;
 		}
 
 	#if UNITY_EDITOR && CONFIGURABLE_ENTER_PLAY_MODE

spine-unity/Assets/Spine/Runtime/spine-unity/Components/SkeletonGraphic.cs
@@ -202,6 +202,7 @@ namespace Spine.Unity {
 		protected override void Awake () {
 
 			base.Awake ();
+			updateMode = updateWhenInvisible;
 			SyncRawImagesWithCanvasRenderers();
 			if (!this.IsValid) {
 #if UNITY_EDITOR

commit 9e9445199a242e11851926fa387ce5954602d2e9

[unity] Fix for UpdateWhenInvisible issue introduced in commit 5cb54bd. Closes #1897. See #1883.

spine-unity/Assets/Spine/Runtime/spine-unity/Components/SkeletonRenderer.cs
@@ -503,7 +503,10 @@ namespace Spine.Unity {
 		}
 
 		public void OnBecameVisible () {
+			UpdateMode previousUpdateMode = updateMode;
 			updateMode = UpdateMode.FullUpdate;
+			if (previousUpdateMode != UpdateMode.FullUpdate)
+				LateUpdate(); // OnBecameVisible is called after LateUpdate()
 		}
 
 		public void OnBecameInvisible () {