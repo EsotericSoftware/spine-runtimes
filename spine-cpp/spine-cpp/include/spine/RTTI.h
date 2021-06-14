34b355eabd2214e0cebba5b1d33cf769a3e5883e
Mario Zechner <badlogicgames@gmail.com>
2021-06-14 15:12:56 +0200

    [cpp] RTTI should not inherit from SpineObject, closes #1870
    Depending on the destruction order of static RTTI instances and SpineExtension instances, we'd call SpineExtension::beforeFree in ~RTTI, when SpineExtension has already been destructed.

--- spine-cpp/spine-cpp/include/spine/RTTI.h
+++ spine-cpp/spine-cpp/include/spine/RTTI.h
@@ -30,10 +30,10 @@
 #ifndef Spine_RTTI_h
 #define Spine_RTTI_h
 
-#include <spine/SpineObject.h>
+#include <spine/dll.h>
 
 namespace spine {
-	class SP_API RTTI : public SpineObject {
+	class SP_API RTTI {
 	public:
 		explicit RTTI(const char *className);
 
