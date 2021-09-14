2d34b84f467feb5ced560a7d2e67c06b1e254d97
Nathan Sweet <nathan.sweet@gmail.com>
2021-09-13 18:28:07 -0600

    [cpp] Simplified setAttachment deform clear logic, fewer parens.

--- spine-cpp/spine-cpp/src/spine/Slot.cpp
+++ spine-cpp/spine-cpp/src/spine/Slot.cpp
@@ -99,15 +99,12 @@ void Slot::setAttachment(Attachment *inValue) {
 		return;
 	}
 
-	if (!inValue || !_attachment) {
+	if (!inValue ||
+		!_attachment ||
+		!inValue->getRTTI().instanceOf(VertexAttachment::rtti) ||
+		!_attachment->getRTTI().instanceOf(VertexAttachment::rtti) ||
+		static_cast<VertexAttachment *>(inValue)->getDeformAttachment() != static_cast<VertexAttachment *>(_attachment)->getDeformAttachment()) {
 		_deform.clear();
-	} else {
-		if (!(inValue->getRTTI().instanceOf(VertexAttachment::rtti)) ||
-			!(_attachment->getRTTI().instanceOf(VertexAttachment::rtti)) ||
-			(static_cast<VertexAttachment *>(inValue)->getDeformAttachment() !=
-			 (static_cast<VertexAttachment *>(_attachment)->getDeformAttachment()))) {
-			_deform.clear();
-		}
 	}
 
 	_attachment = inValue;
