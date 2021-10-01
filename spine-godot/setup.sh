91748c2bbc3c142a511a180dc7446be1a80f56d8
Mario Zechner <badlogicgames@gmail.com>
2021-10-01 14:21:49 +0200

    Add CLion macOS project files

--- spine-godot/setup.sh
+++ spine-godot/setup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -e
 rm -rf godot
-cp -r ../spine-cpp/spine-cpp spine-godot
+cp -r ../spine-cpp/spine-cpp spine_godot
 git clone --depth 1 https://github.com/godotengine/godot.git -b 3.3.4-stable
 cp custom.py godot
+cp -r .idea godot
 ln -s $(pwd)/spine_godot godot/modules/spine_godot
 pushd godot
 scons target=debug --jobs=$(sysctl -n hw.logicalcpu)
