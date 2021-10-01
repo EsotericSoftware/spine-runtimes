b22a043ad5d1755e4c004eb93396bf22a623b171
Mario Zechner <badlogicgames@gmail.com>
2021-10-01 13:44:12 +0200

    Faster compilation by excluding unnecessary modules.

--- spine-godot/setup.sh
+++ spine-godot/setup.sh
@@ -2,8 +2,9 @@
 set -e
 rm -rf godot
 cp -r ../spine-cpp/spine-cpp spine-godot
-git clone --depth 1 https://github.com/godotengine/godot.git -b 3.3.3-stable
+git clone --depth 1 https://github.com/godotengine/godot.git -b 3.3.4-stable
+cp custom.py godot
 ln -s $(pwd)/spine_godot godot/modules/spine_godot
 pushd godot
-scons --jobs=$(sysctl -n hw.logicalcpu)
+scons target=debug --jobs=$(sysctl -n hw.logicalcpu)
 popd
