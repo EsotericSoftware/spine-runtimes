3b061eaa5f631a4ff1dad6179ddf89a9df0143b5
Harald Csaszar <harald.csaszar@pixelcloudgames.com>
2021-11-22 19:12:18 +0100

    [unity][csharp] Moved automated tests out of the spine-unity Assets directory to a UPM package (to prevent potential compile errors upon incomplete setup). Updated spine-csharp tests to be usable as a UPM package.

---
+++ spine-unity/Modules/com.esotericsoftware.spine.spine-unity-tests/Tests/RunAnimationStateTests.cs
@@ -0,0 +1,38 @@
+using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Spine.Unity.Tests {
+	public class RunAnimationStateTests {
+		[Test]
+		public void RunAnimationStateTestsSimplePasses () {
+			AnimationStateTests.logImplementation += Log;
+			AnimationStateTests.failImplementation += Fail;
+			//AnimationStateTests.Main("Assets/SpineTests/spine-csharp-tests/tests/assets/test.json");
+
+			string testJsonFilename = "test";
+			string testJsonPathEnd = "tests/assets/" + testJsonFilename + ".json";
+			var guids = UnityEditor.AssetDatabase.FindAssets(testJsonFilename + " t:textasset");
+			if (guids.Length <= 0) Fail(testJsonFilename + ".json asset not found.");
+
+			foreach (var guid in guids) {
+				string assetPath = UnityEditor.AssetDatabase.GUIDToAssetPath(guid);
+				if (assetPath.EndsWith(testJsonPathEnd)) {
+					AnimationStateTests.Main(assetPath);
+					return;
+				}
+			}
+			Fail(testJsonPathEnd + " not found.");
+		}
+
+		public void Log (string message) {
+			UnityEngine.Debug.Log(message);
+		}
+
+		public void Fail (string message) {
+			Assert.Fail(message);
+		}
+	}
+}
