94d218d0c765a9319334aed04d2f3913e897057e
Mario Zechner <badlogicgames@gmail.com>
2021-12-15 16:34:31 +0100

    [cpp] Sequence timeline.

--- spine-cpp/spine-cpp/include/spine/SequenceTimeline.h
+++ spine-cpp/spine-cpp/include/spine/SequenceTimeline.h
@@ -27,33 +27,51 @@
  * THE SPINE RUNTIMES, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *****************************************************************************/
 
-#ifndef Spine_Property_h
-#define Spine_Property_h
+#ifndef Spine_SequenceTimeline_h
+#define Spine_SequenceTimeline_h
+
+#ifdef SPINE_UE4
+#include "SpinePluginPrivatePCH.h"
+#endif
+
+#include <spine/Timeline.h>
+#include <spine/Sequence.h>
 
 namespace spine {
-	typedef long long PropertyId;
-	enum Property {
-		Property_Rotate = 1 << 0,
-		Property_X = 1 << 1,
-		Property_Y = 1 << 2,
-		Property_ScaleX = 1 << 3,
-		Property_ScaleY = 1 << 4,
-		Property_ShearX = 1 << 5,
-		Property_ShearY = 1 << 6,
-		Property_Rgb = 1 << 7,
-		Property_Alpha = 1 << 8,
-		Property_Rgb2 = 1 << 9,
-		Property_Attachment = 1 << 10,
-		Property_Deform = 1 << 11,
-		Property_Event = 1 << 12,
-		Property_DrawOrder = 1 << 13,
-		Property_IkConstraint = 1 << 14,
-		Property_TransformConstraint = 1 << 15,
-		Property_PathConstraintPosition = 1 << 16,
-		Property_PathConstraintSpacing = 1 << 17,
-		Property_PathConstraintMix = 1 << 18,
-		Property_Sequence = 1 << 19
+	class Attachment;
+
+	class SP_API SequenceTimeline : public Timeline {
+		friend class SkeletonBinary;
+
+		friend class SkeletonJson;
+
+	RTTI_DECL
+
+	public:
+		explicit SequenceTimeline(size_t frameCount, int slotIndex, spine::Attachment *attachment);
+
+		virtual ~SequenceTimeline();
+
+		virtual void
+		apply(Skeleton &skeleton, float lastTime, float time, Vector<Event *> *pEvents, float alpha, MixBlend blend,
+			  MixDirection direction);
+
+		void setFrame(int frame, float time, SequenceMode mode, int index, float delay);
+
+		int getSlotIndex() { return _slotIndex; };
+
+		void setSlotIndex(int inValue) { _slotIndex = inValue; }
+
+		Attachment *getAttachment() { return _attachment; }
+
+	protected:
+		int _slotIndex;
+		Attachment *_attachment;
+
+		static const int ENTRIES = 3;
+		static const int MODE = 1;
+		static const int DELAY = 2;
 	};
 }
 
-#endif /* Spine_Property_h */
+#endif /* Spine_SequenceTimeline_h */
