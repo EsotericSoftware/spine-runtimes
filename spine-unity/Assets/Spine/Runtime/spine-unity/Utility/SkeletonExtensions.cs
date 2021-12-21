EsotericSoftware/spine-runtimes

commit 506a1abf536492a4864a21f5e2863cbbc27c8da8

[unity] Fixed extension method `VertexAttachment.GetLocalVertices` ignoring deform timelines. Closes #1990.

spine-unity/Assets/Spine/Runtime/spine-unity/Utility/SkeletonExtensions.cs
@@ -233,7 +233,7 @@ namespace Spine.Unity {
 			buffer = buffer ?? new Vector2[bufferTargetSize];
 			if (buffer.Length < bufferTargetSize) throw new System.ArgumentException(string.Format("Vector2 buffer too small. {0} requires an array of size {1}. Use the attachment's .WorldVerticesLength to get the correct size.", va.Name, floatsCount), "buffer");
 
-			if (va.Bones == null) {
+			if (va.Bones == null && va.DeformAttachment == null) {
 				var localVerts = va.Vertices;
 				for (int i = 0; i < bufferTargetSize; i++) {
 					int j = i * 2;

commit 7400da8896e0f719a9e2ca0d41cee5e4ddd2818c

[unity] Fixed BoundingBoxFollowerGraphic vertices being 0.0 in some occasions, closes #1999. Problem introduced in commit 506a1ab, see issue #1990.

spine-unity/Assets/Spine/Runtime/spine-unity/Utility/SkeletonExtensions.cs
@@ -233,7 +233,7 @@ namespace Spine.Unity {
 			buffer = buffer ?? new Vector2[bufferTargetSize];
 			if (buffer.Length < bufferTargetSize) throw new System.ArgumentException(string.Format("Vector2 buffer too small. {0} requires an array of size {1}. Use the attachment's .WorldVerticesLength to get the correct size.", va.Name, floatsCount), "buffer");
 
-			if (va.Bones == null && va.DeformAttachment == null) {
+			if (va.Bones == null && slot.Deform.Count == 0) {
 				var localVerts = va.Vertices;
 				for (int i = 0; i < bufferTargetSize; i++) {
 					int j = i * 2;
@@ -299,7 +299,6 @@ namespace Spine.Unity {
 
 namespace Spine {
 	using System;
-	using System.Collections.Generic;
 
 	public struct BoneMatrix {
 		public float a, b, c, d, x, y;