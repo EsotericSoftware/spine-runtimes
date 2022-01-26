25bea5e7b8c6b349fbac63e55c340d51e30fcd0c
Luke Ingram <lingram217@gmail.com>
2022-01-26 14:15:26 -0400

    [ts] Port of additive scale mixing fix, see #2028

--- spine-ts/spine-core/src/Animation.ts
+++ spine-ts/spine-core/src/Animation.ts
@@ -622,8 +622,8 @@ export class ScaleTimeline extends CurveTimeline2 implements BoneTimeline {
 						bone.scaleY = by + (Math.abs(y) * MathUtils.signum(by) - by) * alpha;
 						break;
 					case MixBlend.add:
-						bone.scaleX = (x - bone.data.scaleX) * alpha;
-						bone.scaleY = (y - bone.data.scaleY) * alpha;
+						bone.scaleX += (x - bone.data.scaleX) * alpha;
+						bone.scaleY += (y - bone.data.scaleY) * alpha;
 				}
 			} else {
 				switch (blend) {
@@ -695,7 +695,7 @@ export class ScaleXTimeline extends CurveTimeline1 implements BoneTimeline {
 						bone.scaleX = bx + (Math.abs(x) * MathUtils.signum(bx) - bx) * alpha;
 						break;
 					case MixBlend.add:
-						bone.scaleX = (x - bone.data.scaleX) * alpha;
+						bone.scaleX += (x - bone.data.scaleX) * alpha;
 				}
 			} else {
 				switch (blend) {
@@ -762,7 +762,7 @@ export class ScaleYTimeline extends CurveTimeline1 implements BoneTimeline {
 						bone.scaleY = by + (Math.abs(y) * MathUtils.signum(by) - by) * alpha;
 						break;
 					case MixBlend.add:
-						bone.scaleY = (y - bone.data.scaleY) * alpha;
+						bone.scaleY += (y - bone.data.scaleY) * alpha;
 				}
 			} else {
 				switch (blend) {
