1a51078c1996f428cc13ce6ef4565215b4cd4d30
Mario Zechner <badlogicgames@gmail.com>
2022-04-04 13:41:56 +0200

    [godot] Get rid of symlink

--- spine-godot/setup.sh
+++ spine-godot/setup.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 set -e
-rm -rf godot
-cp -r ../spine-cpp/spine-cpp spine_godot
-git clone --depth 1 https://github.com/godotengine/godot.git -b 3.4.4-stable
-cp custom.py godot
+cp -r ../spine-cpp/spine-cpp godot/modules/spine_godot
+git clone --depth 1 https://github.com/godotengine/godot.git -b 3.4.4-stable godot-copy
+rm -rf godot-copy/.git
+cp -r godot-copy/* godot
 cp -r .idea godot
-ln -s $(pwd)/spine_godot godot/modules/spine_godot
 pushd godot
 scons target=debug --jobs=$(sysctl -n hw.logicalcpu)
 popd
