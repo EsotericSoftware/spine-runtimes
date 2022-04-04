85227351eef492a5b930ae196bd42540dc70f9ee
Mario Zechner <badlogicgames@gmail.com>
2022-04-04 13:44:29 +0200

    [godot] Setup script cleans up.

--- spine-godot/setup.sh
+++ spine-godot/setup.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 set -e
+git clean -x -f -d
 cp -r ../spine-cpp/spine-cpp godot/modules/spine_godot
 git clone --depth 1 https://github.com/godotengine/godot.git -b 3.4.4-stable godot-copy
 rm -rf godot-copy/.git
 cp -r godot-copy/* godot
+rm -rf godot-copy
 cp -r .idea godot
 pushd godot
+scons -Q compiledb
 scons target=debug --jobs=$(sysctl -n hw.logicalcpu)
 popd
