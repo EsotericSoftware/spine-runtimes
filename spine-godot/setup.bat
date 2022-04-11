b277deb26b12f65b4f35bf973fdc8ef5f6fcb1c9
badlogic <badlogicgames@gmail.com>
2022-04-11 10:37:07 +0200

    [godot] Fix CLion config files for Windows.

--- spine-godot/setup.bat
+++ spine-godot/setup.bat
@@ -4,4 +4,4 @@ git clone --depth 1 https://github.com/godotengine/godot.git -b 3.4.4-stable
 xcopy /E /I .idea godot\.idea
 copy custom.py godot
 xcopy /E /I ..\spine-cpp\spine-cpp spine_godot\spine-cpp
-cd godot & scons target=debug compiledb custom_modules=../spine_godot --jobs=8 & scons target=debug custom_modules=../spine_godot --jobs=8 & cd ..
+cd godot & scons target=debug custom_modules=../spine_godot vsproj=yes --jobs=8 & cd ..
