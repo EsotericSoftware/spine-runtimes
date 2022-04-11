58279b2b4a21975569fa06fbd6dd9e3bbb2eafe3
badlogic <badlogicgames@gmail.com>
2022-04-11 22:38:41 +0200

    [godot] Add Live++ support via patch.

--- spine-godot/setup.bat
+++ spine-godot/setup.bat
@@ -4,4 +4,4 @@ git clone --depth 1 https://github.com/godotengine/godot.git -b 3.4.4-stable
 xcopy /E /I .idea godot\.idea
 copy custom.py godot
 xcopy /E /I ..\spine-cpp\spine-cpp spine_godot\spine-cpp
-cd godot & scons target=debug custom_modules=..\spine_godot vsproj=yes --jobs=16 & cd ..
+cd godot & git apply ../livepp.patch scons target=debug custom_modules=..\spine_godot vsproj=yes --jobs=16 & cd ..
