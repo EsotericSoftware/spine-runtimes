b13ec4b1f15311f53956ae702c60c13ee410f92c
badlogic <badlogicgames@gmail.com>
2022-04-12 10:13:19 +0200

    [godot] Path methods.py for Live++

--- spine-godot/setup.bat
+++ spine-godot/setup.bat
@@ -2,5 +2,6 @@ rmdir godot /s /q
 git clone --depth 1 https://github.com/godotengine/godot.git -b 3.4.4-stable
 xcopy /E /I .idea godot\.idea
 copy custom.py godot
+rmdir spine_godot\spine-cpp /s /q
 xcopy /E /I ..\spine-cpp\spine-cpp spine_godot\spine-cpp
-cd godot & git apply ../livepp.patch & scons target=debug custom_modules=..\spine_godot vsproj=yes --jobs=16 & cd ..
+cd godot & git apply ../livepp.patch & scons target=debug custom_modules=..\spine_godot vsproj=yes livepp=%LIVEPP% --jobs=16 & cd ..
