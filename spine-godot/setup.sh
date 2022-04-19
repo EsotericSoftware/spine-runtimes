56893add5e2cacc98e51527ee04287efc3ffe4f9
Mario Zechner <badlogicgames@gmail.com>
2022-04-19 16:50:40 +0200

    [godot] Extract build step into separate shell scripts.

--- spine-godot/setup.sh
+++ spine-godot/setup.sh
@@ -5,6 +5,4 @@ git clone --depth 1 https://github.com/godotengine/godot.git -b 3.4.4-stable
 cp -r .idea godot
 cp custom.py godot
 cp -r ../spine-cpp/spine-cpp spine_godot
-pushd godot
-scons compiledb=yes custom_modules="../spine_godot" -j16
-popd
+./build.sh
