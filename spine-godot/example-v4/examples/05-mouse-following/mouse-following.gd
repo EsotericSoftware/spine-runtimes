9ce8d5171ab38b3e55f9ec86216b576bc6855c30
badlogic <badlogicgames@gmail.com>
2022-04-27 15:27:16 +0200

    [godot] Cleaned-up and added v4 examples.

---
+++ spine-godot/example-v4/examples/05-mouse-following/mouse-following.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+@onready var spineboy: SpineSprite = $Spineboy
+
+func _before_world_transforms_change(_sprite):
+	spineboy.set_global_bone_transform("crosshair", Transform2D(0, get_viewport().get_mouse_position()))
+
+func _ready():
+	spineboy.get_animation_state().set_animation("walk", true, 0)
+	spineboy.get_animation_state().set_animation("aim", true, 1)
+	spineboy.connect("before_world_transforms_change", Callable(self, "_before_world_transforms_change"))
---
+++ spine-godot/example-v4/examples/07-slot-node/slot-node.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+@onready var spineboy: SpineSprite = $Spineboy
+@onready var raptor: SpineSprite = $Spineboy/SlotNodeGun/Raptor
+@onready var tiny_spineboy: SpineSprite = $Spineboy/SlotNodeFrontFist/TinySpineboy
+
+func _ready():
+	var entry = spineboy.get_animation_state().set_animation("run", true, 0)
+	entry.set_time_scale(0.1)
+	raptor.get_animation_state().set_animation("walk", true, 0)
+	tiny_spineboy.get_animation_state().set_animation("walk", true, 0)
