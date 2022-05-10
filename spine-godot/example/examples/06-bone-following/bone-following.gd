dc8c3809863bd699b72891dac90b8118ee947c75
badlogic <badlogicgames@gmail.com>
2022-05-10 12:19:44 +0200

    [godot] Fix bone-following.gd to not connect to "world_transforms_changed"

--- spine-godot/example/examples/06-bone-following/bone-following.gd
+++ spine-godot/example/examples/06-bone-following/bone-following.gd
@@ -2,5 +2,4 @@ extends Node2D
 
 func _ready():
 	var spineboy: SpineSprite = $Spineboy
-	spineboy.get_animation_state().set_animation("walk", true, 0)
-	spineboy.connect("world_transforms_changed", self, "_world_transforms_changed")
+	spineboy.get_animation_state().set_animation("walk", true, 0)	
