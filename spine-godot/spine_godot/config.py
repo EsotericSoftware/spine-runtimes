f5b72c62cf30a4d8c0d5b5519e7ccd230bfe466f
Mario Zechner <badlogicgames@gmail.com>
2022-06-20 16:42:56 +0200

    [godot] Added in-editor doc scafolds.

--- spine-godot/spine_godot/config.py
+++ spine-godot/spine_godot/config.py
@@ -3,3 +3,36 @@ def can_build(env, platform):
 
 def configure(env):
     pass
+
+def get_doc_path():
+    return "docs"
+
+def get_doc_classes():
+    return [
+        "SpineAnimation",
+        "SpineAnimationState",
+        "SpineAnimationTrack",
+        "SpineAtlasResource",
+        "SpineAttachment",
+        "SpineBone",
+        "SpineBoneData",
+        "SpineBoneNode",
+        "SpineConstraintData",
+        "SpineEvent",
+        "SpineIkConstraint",
+        "SpineIkConstraintData",
+        "SpinePathConstraint",
+        "SpinePathConstraintData",
+        "SpineSkeleton",
+        "SpineSkeletonDataResource",
+        "SpineSkeletonFileResource",
+        "SpineSkin",
+        "SpineSlot",
+        "SpineSlotData",
+        "SpineSlotNode",
+        "SpineSprite",
+        "SpineTimeline",
+        "SpineTrackEntry",
+        "SpineTransformConstraint",
+        "SpineTransformConstraintData"        
+    ]
