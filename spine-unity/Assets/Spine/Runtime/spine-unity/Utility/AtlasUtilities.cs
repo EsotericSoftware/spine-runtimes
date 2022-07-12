EsotericSoftware/spine-runtimes

commit 9ecf28b2530e79838aab1817393d3a08d6c5dcf6

[Unity] Fixed Sprite Mesh Type 'Tight' causing issues when used to replace attachments (GetRemappedClone, ToAtlasRegion). Closes #1884. Closes pull request #1994.

spine-unity/Assets/Spine/Runtime/spine-unity/Utility/AtlasUtilities.cs
@@ -193,18 +193,15 @@ namespace Spine.Unity.AttachmentTools {
 			region.index = -1;
 			region.degrees = s.packed && s.packingRotation != SpritePackingRotation.None ? 90 : 0;
 
-			// World space units
-			Bounds bounds = s.bounds;
-			Vector2 boundsMin = bounds.min, boundsMax = bounds.max;
-
 			// Texture space/pixel units
-			Rect spineRect = s.rect.SpineUnityFlipRect(s.texture.height);
+			Rect spineRect = s.textureRect.SpineUnityFlipRect(s.texture.height);
+			Rect originalRect = s.rect;
 			region.width = (int)spineRect.width;
-			region.originalWidth = (int)spineRect.width;
+			region.originalWidth = (int)originalRect.width;
 			region.height = (int)spineRect.height;
-			region.originalHeight = (int)spineRect.height;
-			region.offsetX = spineRect.width * (0.5f - InverseLerp(boundsMin.x, boundsMax.x, 0));
-			region.offsetY = spineRect.height * (0.5f - InverseLerp(boundsMin.y, boundsMax.y, 0));
+			region.originalHeight = (int)originalRect.height;
+			region.offsetX = s.textureRectOffset.x;
+			region.offsetY = s.textureRectOffset.y;
 
 			if (isolatedTexture) {
 				region.u = 0;

commit 766105189f1b017080cfe38196af1c66bb70a584

[unity] Fixed Sprite.ToAtlasRegion and SpriteAttacher example script ignoring pivot in 4.1. Closes #2109. Issue was introduced in commit 9ecf28b, see #1884.

spine-unity/Assets/Spine/Runtime/spine-unity/Utility/AtlasUtilities.cs
@@ -193,6 +193,10 @@ namespace Spine.Unity.AttachmentTools {
 			region.index = -1;
 			region.degrees = s.packed && s.packingRotation != SpritePackingRotation.None ? 90 : 0;
 
+			// World space units
+			Bounds bounds = s.bounds;
+			Vector2 boundsMin = bounds.min, boundsMax = bounds.max;
+
 			// Texture space/pixel units
 			Rect spineRect = s.textureRect.SpineUnityFlipRect(s.texture.height);
 			Rect originalRect = s.rect;
@@ -200,8 +204,8 @@ namespace Spine.Unity.AttachmentTools {
 			region.originalWidth = (int)originalRect.width;
 			region.height = (int)spineRect.height;
 			region.originalHeight = (int)originalRect.height;
-			region.offsetX = s.textureRectOffset.x;
-			region.offsetY = s.textureRectOffset.y;
+			region.offsetX = s.textureRectOffset.x + spineRect.width * (0.5f - InverseLerp(boundsMin.x, boundsMax.x, 0));
+			region.offsetY = s.textureRectOffset.y + spineRect.height * (0.5f - InverseLerp(boundsMin.y, boundsMax.y, 0));
 
 			if (isolatedTexture) {
 				region.u = 0;