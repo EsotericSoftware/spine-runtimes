ba5f89f3610381a9111c74fef3481467b5c3d25c
Mario Zechner <badlogicgames@gmail.com>
2022-09-15 07:46:33 +0200

    [ts] Correct order of UV assignements in RegionAttachment.updateRegion()

--- spine-ts/spine-core/src/attachments/RegionAttachment.ts
+++ spine-ts/spine-core/src/attachments/RegionAttachment.ts
@@ -131,14 +131,14 @@ export class RegionAttachment extends Attachment implements HasTextureRegion {
 		offset[7] = localYCos + localX2Sin;
 
 		if (region.degrees == 90) {
+			uvs[0] = region.u2;
+			uvs[1] = region.v2;
 			uvs[2] = region.u;
 			uvs[3] = region.v2;
 			uvs[4] = region.u;
 			uvs[5] = region.v;
 			uvs[6] = region.u2;
 			uvs[7] = region.v;
-			uvs[0] = region.u2;
-			uvs[1] = region.v2;
 		} else {
 			uvs[0] = region.u;
 			uvs[1] = region.v2;
