69cec06c23b519008d57bd6ab53ae64a954eda2d
Mario Zechner <badlogicgames@gmail.com>
2022-12-13 20:53:08 +0100

    [phaser] Clean-up rendering code.

--- spine-ts/spine-phaser/src/SpineGameObject.ts
+++ spine-ts/spine-phaser/src/SpineGameObject.ts
@@ -67,15 +67,7 @@ export class SpineGameObject extends ComputedSizeMixin(DepthMixin(FlipMixin(Scro
 
 		camera.addToRenderList(src);
 		let transform = Phaser.GameObjects.GetCalcMatrix(src, camera, parentMatrix).calc;
-		/*let x = transform.tx;
-		let y = transform.ty;
-		let scaleX = transform.scaleX;
-		let scaleY = transform.scaleY;
-		let rotation = transform.rotationNormalized;
-		let cosRotation = Math.cos(rotation);
-		let sinRotation = Math.sin(rotation);*/
 		let a = transform.a, b = transform.b, c = transform.c, d = transform.d, tx = transform.tx, ty = transform.ty;
-
 		sceneRenderer.drawSkeleton(this.skeleton, this.premultipliedAlpha, -1, -1, (vertices, numVertices, stride) => {
 			for (let i = 0; i < numVertices; i += stride) {
 				let vx = vertices[i];
