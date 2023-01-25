9da2be9f6fd234087b4ce7fc1142ba3747e8273b
Mario Zechner <badlogicgames@gmail.com>
2023-01-25 13:17:30 +0100

    [godot] Fix web templates build for Godot 4.0

--- spine-godot/build/build-templates-v4.sh
+++ spine-godot/build/build-templates-v4.sh
@@ -93,10 +93,10 @@ elif [ "$platform" = "ios" ]; then
 elif [ "$platform" = "web" ]; then
 	# --- WEB ---
 	# generates webassembly_debug.zip, webassembly_release.zip
-	scons platform=javascript tools=no target=template_release custom_modules="../spine_godot" --jobs=$cpus
-	scons platform=javascript tools=no target=template_debug custom_modules="../spine_godot" --jobs=$cpus
-	mv bin/godot.javascript.opt.zip bin/webassembly_release.zip
-	mv bin/godot.javascript.opt.debug.zip bin/webassembly_debug.zip
+	scons platform=web tools=no target=template_release custom_modules="../spine_godot" --jobs=$cpus
+	scons platform=web tools=no target=template_debug custom_modules="../spine_godot" --jobs=$cpus
+	mv bin/godot.web.template_release.wasm32.zip bin/web_release.zip
+	mv bin/godot.web.template_debug.wasm32.zip bin/web_debug.zip
 elif [ "$platform" = "android" ]; then
 	# --- ANROID ---
 	# generates android_release.apk, android_debug.apk, android_source.zip
