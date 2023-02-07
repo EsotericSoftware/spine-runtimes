0f595e7fbeeee44a30be7831f121d2cd6b5fbfd6
badlogic <badlogicgames@gmail.com>
2023-02-07 10:34:33 +0100

    [ue4] Add setup scripts.

---
+++ spine-ue4/setup.bat
@@ -0,0 +1,9 @@
+@echo off
+rmdir Plugins\SpinePlugin\Source\SpinePlugin\Public\spine-cpp /s /q
+xcopy /E /I ..\spine-cpp\spine-cpp Plugins\SpinePlugin\Source\SpinePlugin\Public\spine-cpp  || goto error
+goto done
+
+:error
+@echo Couldn^'t setup spine-ue4
+
+:done
