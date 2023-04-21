2cc7667a18028e9985e472c189fc3908b38f2a0f
Mario Zechner <badlogicgames@gmail.com>
2023-04-21 11:33:35 +0200

    [phaser] Add vanilla JS and NPM/TS examples, fix typings.

---
+++ spine-ts/spine-phaser/example/npm/index.ts
@@ -0,0 +1,30 @@
+import {Scene} from "phaser"
+import {SpineGameObject, SpineGameObjectBoundsProvider, SpinePlugin} from "@esotericsoftware/spine-phaser"
+
+class SpineDemo extends Scene {
+    preload() {
+        this.load.spineBinary("spineboy-data", "assets/spineboy-pro.skel");
+        this.load.spineAtlas("spineboy-atlas", "assets/spineboy-pma.atlas");
+    }
+
+    create() {
+        let spineboy = this.add.spine(400, 500, 'spineboy-data', "spineboy-atlas");
+        this.make
+        spineboy.scale = 0.5;
+        spineboy.animationState.setAnimation(0, "walk", true);
+    }
+}
+
+var config = {    
+    width: 800,
+    height: 600,
+    type: Phaser.WEBGL,
+    scene: [SpineDemo],
+    plugins: {
+        scene: [
+            { key: "spine.SpinePlugin", plugin: SpinePlugin, mapping: "spine" }
+        ]
+    }
+};
+
+let game = new Phaser.Game(config);
--- spine-ts/spine-phaser/src/index.ts
+++ spine-ts/spine-phaser/src/index.ts
@@ -7,3 +7,25 @@ export * from "@esotericsoftware/spine-webgl";
 import { SpinePlugin } from "./SpinePlugin";
 (window as any).spine = { SpinePlugin: SpinePlugin };
 (window as any)["spine.SpinePlugin"] = SpinePlugin;
+
+import { SpineGameObject, SpineGameObjectBoundsProvider } from "./SpineGameObject";
+
+declare global {
+    namespace Phaser.Loader {
+        export interface LoaderPlugin {
+            spineJson(key: string, url: string, xhrSettings?: Phaser.Types.Loader.XHRSettingsObject): LoaderPlugin;
+            spineBinary(key: string, url: string, xhrSettings?: Phaser.Types.Loader.XHRSettingsObject): LoaderPlugin;
+            spineAtlas(key: string, url: string, premultipliedAlpha?: boolean, xhrSettings?: Phaser.Types.Loader.XHRSettingsObject): LoaderPlugin;
+        }
+    }
+
+    namespace Phaser.GameObjects {
+        export interface GameObjectFactory {
+            spine(x: number, y: number, dataKey: string, atlasKey: string, boundsProvider?: SpineGameObjectBoundsProvider): SpineGameObject;
+        }
+
+        export interface GameObjectCreator {
+            spine(config: any, addToScene: boolean): SpineGameObject;
+        }
+    }
+}
