125d7c9903f3157e3a46191f0c976ac5137aeab1
Mario Zechner <badlogicgames@gmail.com>
2023-04-24 10:33:40 +0200

    [phaser] Example clean-up

--- spine-ts/spine-phaser/example/typescript/index.ts
+++ spine-ts/spine-phaser/example/typescript/index.ts
@@ -8,14 +8,14 @@ class SpineDemo extends Scene {
     }
 
     create() {
-        let spineboy = this.add.spine(400, 500, 'spineboy-data', "spineboy-atlas");
+        const spineboy = this.add.spine(400, 500, 'spineboy-data', "spineboy-atlas");
         this.make
         spineboy.scale = 0.5;
         spineboy.animationState.setAnimation(0, "walk", true);
     }
 }
 
-var config = {    
+const config = {    
     width: 800,
     height: 600,
     type: Phaser.WEBGL,
@@ -27,4 +27,4 @@ var config = {
     }
 };
 
-let game = new Phaser.Game(config);
+new Phaser.Game(config);
