f1c13462c1f1307a13425d52fe6c0477423a3c23
Mario Zechner <badlogicgames@gmail.com>
2023-04-24 10:15:31 +0200

    Formatting

--- spine-ts/spine-webgl/src/GLTexture.ts
+++ spine-ts/spine-webgl/src/GLTexture.ts
@@ -56,7 +56,7 @@ export class GLTexture extends Texture implements Disposable, Restorable {
 	}
 
 	static validateMagFilter (magFilter: TextureFilter) {
-		switch (magFilter) {			
+		switch (magFilter) {
 			case TextureFilter.MipMapLinearLinear:
 			case TextureFilter.MipMapLinearNearest:
 			case TextureFilter.MipMapNearestLinear:
@@ -68,7 +68,7 @@ export class GLTexture extends Texture implements Disposable, Restorable {
 	}
 
 	static usesMipMaps (filter: TextureFilter) {
-		switch (filter) {			
+		switch (filter) {
 			case TextureFilter.MipMapLinearLinear:
 			case TextureFilter.MipMapLinearNearest:
 			case TextureFilter.MipMapNearestLinear:
