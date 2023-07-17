EsotericSoftware/spine-runtimes

commit d4cc16315f0698f286726a0b4f7637eb756803a3

[unity] Fixed Timeline SpineAnimationStateClip ignoring seek and loop. Closes #2316. Removed redundant multiple skeleton updates when multiple animations are started.

spine-unity/Modules/com.esotericsoftware.spine.timeline/Runtime/SpineAnimationState/SpineAnimationStateMixerBehaviour.cs
@@ -165,7 +165,7 @@ namespace Spine.Unity.Playables {
 			for (int i = 0; i < inputCount; i++) {
 				float lastInputWeight = lastInputWeights[i];
 				float inputWeight = playable.GetInputWeight(i);
-				bool clipStarted = lastInputWeight == 0 && inputWeight > 0;
+				bool clipStarted = (lastInputWeight == 0 && inputWeight > 0) || info.seekOccurred || info.timeLooped;
 				if (inputWeight > 0)
 					anyClipPlaying = true;
 				lastInputWeights[i] = inputWeight;
@@ -231,10 +231,9 @@ namespace Spine.Unity.Playables {
 
 						timelineStartedTrackEntry = trackEntry;
 					}
-					//else Debug.LogWarningFormat("Animation named '{0}' not found", clipData.animationName);
 				}
-
-				// Ensure that the first frame ends with an updated mesh.
+			}
+			if (numStartingClips > 0) {
 				if (skeletonAnimation) {
 					skeletonAnimation.Update(0);
 					skeletonAnimation.LateUpdate();

commit 0ca3c8e50702cbd37b8acec14d0b938054e22e1d

[unity] Fixed a Timeline SpineAnimationStateClip related bug introduced in commit d4cc1631. Closes #2316.

spine-unity/Modules/com.esotericsoftware.spine.timeline/Runtime/SpineAnimationState/SpineAnimationStateMixerBehaviour.cs
@@ -165,7 +165,7 @@ namespace Spine.Unity.Playables {
 			for (int i = 0; i < inputCount; i++) {
 				float lastInputWeight = lastInputWeights[i];
 				float inputWeight = playable.GetInputWeight(i);
-				bool clipStarted = (lastInputWeight == 0 && inputWeight > 0) || info.seekOccurred || info.timeLooped;
+				bool clipStarted = (inputWeight > 0) && (lastInputWeight == 0 || info.seekOccurred || info.timeLooped);
 				if (inputWeight > 0)
 					anyClipPlaying = true;
 				lastInputWeights[i] = inputWeight;