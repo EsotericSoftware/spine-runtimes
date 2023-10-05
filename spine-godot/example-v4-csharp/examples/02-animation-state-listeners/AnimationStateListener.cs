b8eda5e27bb7f5e7af86788bb247dc4bf3512a4d
Mario Zechner <badlogicgames@gmail.com>
2023-10-05 11:52:47 +0200

    [godot] Use GD.Print instead of Console.WriteLine.

--- spine-godot/example-v4-csharp/examples/02-animation-state-listeners/AnimationStateListener.cs
+++ spine-godot/example-v4-csharp/examples/02-animation-state-listeners/AnimationStateListener.cs
@@ -10,28 +10,28 @@ public partial class AnimationStateListener : Node2D
 		spineboy.AnimationStarted += (sprite, animationState, trackEntry) =>
 		{
 			var spineTrackEntry = trackEntry as SpineTrackEntry;
-			Console.WriteLine("Animation started: " + spineTrackEntry.GetAnimation().GetName());
+			GD.Print("Animation started: " + spineTrackEntry.GetAnimation().GetName());
 		};
 		spineboy.AnimationInterrupted += (sprite, animationState, trackEntry) =>
 		{
 			var spineTrackEntry = trackEntry as SpineTrackEntry;
-			Console.WriteLine("Animation interrupted: " + spineTrackEntry.GetAnimation().GetName());
+			GD.Print("Animation interrupted: " + spineTrackEntry.GetAnimation().GetName());
 		};
 		spineboy.AnimationCompleted += (sprite, animationState, trackEntry) =>
 		{
 			var spineTrackEntry = trackEntry as SpineTrackEntry;
-			Console.WriteLine("Animation completed: " + spineTrackEntry.GetAnimation().GetName());
+			GD.Print("Animation completed: " + spineTrackEntry.GetAnimation().GetName());
 		};
 		spineboy.AnimationDisposed += (sprite, animationState, trackEntry) =>
 		{
 			var spineTrackEntry = trackEntry as SpineTrackEntry;
-			Console.WriteLine("Animation disposed: " + spineTrackEntry.GetAnimation().GetName());
+			GD.Print("Animation disposed: " + spineTrackEntry.GetAnimation().GetName());
 		};
 		spineboy.AnimationEvent += (sprite, animationState, trackEntry, eventObject) =>
 		{
 			var spineTrackEntry = trackEntry as SpineTrackEntry;
 			var spineEvent = eventObject as SpineEvent;
-			Console.WriteLine("Animation event: " + spineTrackEntry.GetAnimation().GetName() + ", " + spineEvent.GetData().GetEventName());
+			GD.Print("Animation event: " + spineTrackEntry.GetAnimation().GetName() + ", " + spineEvent.GetData().GetEventName());
 			if (spineEvent.GetData().GetEventName() == "footstep")
 				footStepAudio.Play();
 		};
