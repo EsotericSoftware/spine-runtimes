92986788c1402d814f69b1a6092446dcfc918a33
Nathan Sweet <nathan.sweet@gmail.com>
2024-01-10 15:12:47 -0400

    [libgdx] Changed SimpleTest1 back to JSON.

--- spine-libgdx/spine-libgdx-tests/src/com/esotericsoftware/spine/SimpleTest1.java
+++ spine-libgdx/spine-libgdx-tests/src/com/esotericsoftware/spine/SimpleTest1.java
@@ -61,9 +61,9 @@ public class SimpleTest1 extends ApplicationAdapter {
 		debugRenderer.setRegionAttachments(false);
 
 		atlas = new TextureAtlas(Gdx.files.internal("spineboy/spineboy-pma.atlas"));
-		SkeletonBinary json = new SkeletonBinary(atlas); // This loads skeleton JSON data, which is stateless.
+		SkeletonJson json = new SkeletonJson(atlas); // This loads skeleton JSON data, which is stateless.
 		json.setScale(0.6f); // Load the skeleton at 60% the size it was in Spine.
-		SkeletonData skeletonData = json.readSkeletonData(Gdx.files.internal("spineboy/spineboy-pro.skel"));
+		SkeletonData skeletonData = json.readSkeletonData(Gdx.files.internal("spineboy/spineboy-pro.json"));
 
 		skeleton = new Skeleton(skeletonData); // Skeleton holds skeleton state (bone positions, slot attachments, etc).
 		skeleton.setPosition(250, 20);
