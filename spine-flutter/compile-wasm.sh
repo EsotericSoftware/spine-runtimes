ac7f0654a6eb146a697393f76ff3130c11eb187e
Mario Zechner <badlogicgames@gmail.com>
2024-04-19 14:43:00 +0200

    [cpp][flutter] Added spine-lite-cpp, changed spine-flutter to use spine-lite-cpp.

--- spine-flutter/compile-wasm.sh
+++ spine-flutter/compile-wasm.sh
@@ -22,7 +22,7 @@ em++ \
 	--no-entry \
 	--extern-pre-js pre.js \
 	-s EXPORT_NAME=libspine_flutter \
-	src/spine_flutter.cpp `find src/spine-cpp/src -type f` \
+	src/spine-cpp-lite/spine-cpp-lite.cpp `find src/spine-cpp/src -type f` \
 	-o lib/assets/libspine_flutter.js
 	ls -lah lib/assets
 rm pre.js
--- spine-flutter/macos/Classes/spine_flutter.cpp
+++ spine-flutter/macos/Classes/spine_flutter.cpp
@@ -1,3 +1,3 @@
 // Relative import to be able to reuse the C sources.
 // See the comment in ../{projectName}}.podspec for more information.
-#include "../../src/spine_flutter.cpp"
+#include "../../src/spine-cpp-lite/spine-cpp-lite.cpp"
