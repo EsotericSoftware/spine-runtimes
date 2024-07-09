931f78175f71c8c1293194e54eb93aed51014b36
Mario Zechner <badlogicgames@gmail.com>
2024-07-09 10:49:13 +0200

    [ts] Improved publish.sh

--- spine-ts/publish.sh
+++ spine-ts/publish.sh
@@ -1,25 +1,25 @@
 #!/bin/sh
 set -e
 
-if [ ! "$#" -eq 2 ]; then
-	echo "Usage: ./publish.sh <last-version> <new-version>"
-	exit
-else
-	lastVersion=${1%/}
-	newVersion=${2%/}
-	echo "last version: $lastVersion"
-	echo "new version: $newVersion"
-fi
+currentVersion=$(grep -o '"version": "[^"]*' package.json | grep -o '[^"]*$')
+major=$(echo "$currentVersion" | cut -d. -f1)
+minor=$(echo "$currentVersion" | cut -d. -f2)
+patch=$(echo "$currentVersion" | cut -d. -f3)
+newPatch=$((patch + 1))
+newVersion="$major.$minor.$newPatch"
 
-sed -i '' "s/$lastVersion/$newVersion/" package.json
-sed -i '' "s/$lastVersion/$newVersion/" spine-canvas/package.json
-sed -i '' "s/$lastVersion/$newVersion/" spine-canvaskit/package.json
-sed -i '' "s/$lastVersion/$newVersion/" spine-core/package.json
-sed -i '' "s/$lastVersion/$newVersion/" spine-phaser/package.json
-sed -i '' "s/$lastVersion/$newVersion/" spine-pixi/package.json
-sed -i '' "s/$lastVersion/$newVersion/" spine-player/package.json
-sed -i '' "s/$lastVersion/$newVersion/" spine-threejs/package.json
-sed -i '' "s/$lastVersion/$newVersion/" spine-webgl/package.json
+echo "current version: $currentVersion"
+echo "new version: $newVersion"
+
+sed -i '' "s/$currentVersion/$newVersion/" package.json
+sed -i '' "s/$currentVersion/$newVersion/" spine-canvas/package.json
+sed -i '' "s/$currentVersion/$newVersion/" spine-canvaskit/package.json
+sed -i '' "s/$currentVersion/$newVersion/" spine-core/package.json
+sed -i '' "s/$currentVersion/$newVersion/" spine-phaser/package.json
+sed -i '' "s/$currentVersion/$newVersion/" spine-pixi/package.json
+sed -i '' "s/$currentVersion/$newVersion/" spine-player/package.json
+sed -i '' "s/$currentVersion/$newVersion/" spine-threejs/package.json
+sed -i '' "s/$currentVersion/$newVersion/" spine-webgl/package.json
 
 rm package-lock.json
 rm -rf node_modules/@esotericsoftware
