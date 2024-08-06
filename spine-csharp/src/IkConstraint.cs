b17595db8a124f729bf02ccfdbf6c5fb977ca783
Harald Csaszar <harald.csaszar@pixelcloudgames.com>
2024-08-06 18:22:54 +0200

    [csharp] Port of commit 853b5ee. Fix NaN in IKConstraint due to fp precision. dd - r * r might result in a negative value, even if dd == r * r, depending on compiler optimizations and operation order.

--- spine-csharp/src/IkConstraint.cs
+++ spine-csharp/src/IkConstraint.cs
@@ -328,7 +328,7 @@ namespace Spine {
 					q = -(c1 + q) * 0.5f;
 					float r0 = q / c2, r1 = c / q;
 					float r = Math.Abs(r0) < Math.Abs(r1) ? r0 : r1;
-					if (r * r <= dd) {
+					if (dd - r * r >= 0) {
 						y = (float)Math.Sqrt(dd - r * r) * bendDir;
 						a1 = ta - (float)Math.Atan2(y, r);
 						a2 = (float)Math.Atan2(y / psy, (r - l1) / psx);
