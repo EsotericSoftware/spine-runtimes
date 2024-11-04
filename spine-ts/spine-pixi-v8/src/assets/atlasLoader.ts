cad61bbc845435b0c7434bd7f2ad6177ba8c2e3b
Davide Tantillo <iamdjj@gmail.com>
2024-11-04 11:16:45 +0100

    Aligned atlas loader to use texturePreference for bundles.

--- spine-ts/spine-pixi-v8/src/assets/atlasLoader.ts
+++ spine-ts/spine-pixi-v8/src/assets/atlasLoader.ts
@@ -34,17 +34,31 @@ import {
 	ExtensionType,
 	LoaderParserPriority,
 	path,
+	Resolver,
 	TextureSource
 } from 'pixi.js';
 import { SpineTexture } from '../SpineTexture';
 import { TextureAtlas } from '@esotericsoftware/spine-core';
 
-import type { AssetExtension, Loader, ResolvedAsset, Texture } from 'pixi.js';
+import type { AssetExtension, Loader, ResolvedAsset, Texture, UnresolvedAsset } from 'pixi.js';
 
 type RawAtlas = string;
 
 const spineTextureAtlasLoader: AssetExtension<RawAtlas | TextureAtlas, ISpineAtlasMetadata> = {
 	extension: ExtensionType.Asset,
+
+	resolver: {
+		test: (value: string): boolean => checkExtension(value, ".atlas"),
+		parse: (value: string): UnresolvedAsset => {
+			const split = value.split('.');
+
+			return {
+				resolution: parseFloat(Resolver.RETINA_PREFIX?.exec(value)?.[1] ?? '1'),
+				format: split[split.length - 2],
+				src: value,
+			};
+		},
+	},
 
 	loader: {
 		extension: {
