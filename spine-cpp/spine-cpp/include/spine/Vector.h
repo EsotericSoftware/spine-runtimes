ce9b0b38fe8f7aee22e7cc5087fe3b9d7e2872c1
James Chen <dumganhar@gmail.com>
2025-03-09 13:28:10 +0800

    Format code

--- spine-cpp/spine-cpp/include/spine/Vector.h
+++ spine-cpp/spine-cpp/include/spine/Vector.h
@@ -86,10 +86,10 @@ namespace spine {
 					construct(_buffer + i, defaultValue);
 				}
 			} else {
-	            for (size_t i = _size; i < oldSize; i++) {
-	                destroy(_buffer + i);
-	            }
-	        }
+				for (size_t i = _size; i < oldSize; i++) {
+					destroy(_buffer + i);
+				}
+			}
 		}
 
 		inline void ensureCapacity(size_t newCapacity = 0) {
